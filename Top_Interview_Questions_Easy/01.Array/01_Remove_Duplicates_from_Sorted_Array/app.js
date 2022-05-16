fd8cf21e804a79844096bfd0b8a57241880a958d
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-16 15:31:42 +0700

    feat(array): :rocket: add question 26.Remove Duplicates from Sorted Array
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ Top_Interview_Questions_Easy/01.Array/01_Remove_Duplicates_from_Sorted_Array/app.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let current = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[current] = nums[i];
+      current++;
+    }
+  }
+  return [current, nums.slice(0, current).join(' ')];
+};
+
+var nums = [1, 1, 2];
+console.log(removeDuplicates(nums));
+var nums = [1, 2];
+console.log(removeDuplicates(nums));
+var nums = [1, 1, 1];
+console.log(removeDuplicates(nums));
+var nums = [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 4, 5, 5, 7, 7, 8, 9, 100];
+console.log(removeDuplicates(nums));
