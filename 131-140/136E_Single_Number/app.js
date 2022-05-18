0889f64fdcd4f6aa3591475af3f6e5c74bc6ddee
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-18 09:56:39 +0700

    feat(array): :sparkles: add 136.Single Number

---
+++ 131-140/136E_Single_Number/app.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/single-number/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let num = 0;
+  for (let i = 0; i < nums.length; i++) {
+    num ^= nums[i];
+  }
+  return num;
+};
+
+var nums = [2, 2, 1];
+console.log(nums, singleNumber(nums));
+
+nums = [4, 1, 2, 1, 2];
+console.log(nums, singleNumber(nums));
+
+nums = [1];
+console.log(nums, singleNumber(nums));
