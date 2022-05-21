667c404f9de4f2d41571d68e4484da50346ced82
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-21 11:45:04 +0700

    feat(array): :zap: add extra-space solution 48.Rotate Image

---
+++ 041-050/048M_Rotate_Image/extra-space.js
@@ -0,0 +1,61 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length;
+  let temp = [];
+  for (let i = 0; i < n; i++) {
+    temp[i] = [];
+  }
+
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < n; c++) {
+      temp[c][n - 1 - r] = matrix[r][c];
+    }
+  }
+
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < n; c++) {
+      matrix[r][c] = temp[r][c];
+    }
+  }
+};
+
+var compare = function (a, b) {
+  return a.flat().join(' ') === b.flat().join(' ');
+};
+
+var print = function (matrix) {
+  matrix.forEach((rows) => console.log(rows.join(' ')));
+};
+
+var matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+var expect = [
+  [7, 4, 1],
+  [8, 5, 2],
+  [9, 6, 3],
+];
+rotate(matrix);
+print(matrix);
+console.log(compare(expect, matrix));
+
+matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+expect = [
+  [15, 13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7, 10, 11],
+];
+rotate(matrix);
+print(matrix);
+console.log(compare(expect, matrix));
