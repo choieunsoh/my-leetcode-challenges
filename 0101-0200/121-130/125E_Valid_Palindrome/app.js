546b82767f371b1de6a166339b72a1f882d6cea4
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-21 17:44:16 +0700

    feat(strings): :sparkles: add 125. Valid Palindrome

---
+++ 0101-0200/121-130/125E_Valid_Palindrome/app.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/valid-palindrome/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+var s = 'A man, a plan, a canal: Panama';
+var expect = true;
+var result = isPalindrome(s);
+console.log(result, result === expect);
+
+var s = 'race a car';
+var expect = false;
+var result = isPalindrome(s);
+console.log(result, result === expect);
+
+var s = ' ';
+var expect = true;
+var result = isPalindrome(s);
+console.log(result, result === expect);
+
+var s = '.,';
+var expect = true;
+var result = isPalindrome(s);
+console.log(result, result === expect);
