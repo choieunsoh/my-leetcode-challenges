298b6f6938b7bc07582a4f2cda543b5e008b66f5
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-24 08:36:53 +0700

    feat(others): :sparkles: add 268. Missing Number

---
+++ 0201-0300/261-270/268E_Missing_Number/app.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/missing-number/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const sum = nums.reduce((sum, num) => sum + num, 0);
+  return (nums.length * (nums.length + 1)) / 2 - sum;
+};
+
+var nums = [3, 0, 1];
+var expect = 2;
+console.log(missingNumber(nums), expect);
+
+var nums = [0, 1];
+var expect = 2;
+console.log(missingNumber(nums), expect);
+
+var nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+var expect = 8;
+console.log(missingNumber(nums), expect);
---
+++ 0201-0300/261-270/268E_Missing_Number/set.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/missing-number/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const numbers = new Set(nums);
+  for (let i = 0; i < nums.length; i++) {
+    if (!numbers.has(i)) return i;
+  }
+  return nums.length;
+};
+
+var nums = [3, 0, 1];
+var expect = 2;
+console.log(missingNumber(nums), expect);
+
+var nums = [0, 1];
+var expect = 2;
+console.log(missingNumber(nums), expect);
+
+var nums = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+var expect = 8;
+console.log(missingNumber(nums), expect);
