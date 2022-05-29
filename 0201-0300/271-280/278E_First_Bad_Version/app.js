7cc9f0ee4bb7df0e2a32c88c5123964db0879191
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-05-29 23:26:59 +0700

    feat(sorting-searching): :sparkles: add 278. First Bad Version

---
+++ 0201-0300/271-280/278E_First_Bad_Version/app.js
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/first-bad-version/
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    if (n === 1) return 1;
+
+    let left = 1;
+    let right = n;
+
+    while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      if (isBadVersion(mid)) {
+        if (isBadVersion(mid - 1) === false) {
+          return mid;
+        } else {
+          right = mid - 1;
+        }
+      } else {
+        if (isBadVersion(mid + 1) === true) {
+          return mid + 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    return 1;
+  };
+};
