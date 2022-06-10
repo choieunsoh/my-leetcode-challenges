97dab303802d9e9aa27afbcd99046cc2e9014dc3
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-06-10 20:21:32 +0700

    feat(sql): :sparkles: update SQL I day 3

--- SQL_I/Day_3_String_Processing_Functions/02E_Group_Sold_Products_By_The_Date/app.sql
+++ SQL_I/Day_3_String_Processing_Functions/02E_Group_Sold_Products_By_The_Date/app.sql
@@ -1,1 +1,11 @@
+-- https://leetcode.com/problems/group-sold-products-by-the-date/
 -- 1484. Group Sold Products By The Date
+WITH temp AS (
+  SELECT DISTINCT * FROM Activities
+)
+SELECT sell_date
+,COUNT(DISTINCT product) AS num_sold
+,STRING_AGG(product,',') WITHIN GROUP (ORDER BY product) AS products
+FROM temp
+GROUP BY sell_date
+ORDER BY sell_date
--- SQL_I/Day_3_String_Processing_Functions/03E_Patients_With_a_Condition/app.sql
+++ SQL_I/Day_3_String_Processing_Functions/03E_Patients_With_a_Condition/app.sql
@@ -1,1 +1,7 @@
+-- https://leetcode.com/problems/patients-with-a-condition/
 -- 1527. Patients With a Condition
+SELECT patient_id,patient_name,conditions
+FROM Patients 
+WHERE conditions LIKE 'DIAB1%'
+OR conditions LIKE '% DIAB1%'
+ORDER BY patient_id
