9d13a592e4ea5625ae70595fc2b79d1896d4d957
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-01 09:19:50 +0700

    feat(recursion): :sparkles: add recursive solution for 189. Pascal's Triangle II

---
+++ 0101-0200/111-120/119E_Pascals_Triangle_II/recursive.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/pascals-triangle-ii/
+// 189. Pascal's Triangle II
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+  if (rowIndex === 0) return [1];
+
+  const row = Array(rowIndex + 1).fill(1);
+  if (rowIndex > 1) {
+    const prevRow = getRow(rowIndex - 1);
+    for (let i = 1; i < rowIndex; i++) {
+      row[i] = prevRow[i - 1] + prevRow[i];
+    }
+  }
+  return row;
+};
+
+var rowIndex = 3;
+var expected = [1, 3, 3, 1];
+var result = getRow(rowIndex);
+console.log(result.join(' '), result.join() === expected.join());
+
+var rowIndex = 0;
+var expected = [1];
+var result = getRow(rowIndex);
+console.log(result.join(' '), result.join() === expected.join());
+
+var rowIndex = 1;
+var expected = [1, 1];
+var result = getRow(rowIndex);
+console.log(result.join(' '), result.join() === expected.join());
+
+var rowIndex = 10;
+var expected = [1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1];
+var result = getRow(rowIndex);
+console.log(result.join(' '), result.join() === expected.join());
