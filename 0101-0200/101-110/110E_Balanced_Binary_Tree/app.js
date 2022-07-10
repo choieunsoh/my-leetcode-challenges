8bcd005f8042a13bc76e8baed3a173176fb27ce6
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-10 22:44:19 +0700

    feat(trees): :sparkles: add 110. Balanced Binary Tree - tree, dfs

---
+++ 0101-0200/101-110/110E_Balanced_Binary_Tree/app.js
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+// 110. Balanced Binary Tree
+const { TreeNode, createTree } = require('../../../_utils/tree');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  function getHeight(node) {
+    if (!node) return 0;
+
+    const leftHeight = getHeight(node.left);
+    if (leftHeight < 0) return leftHeight;
+
+    const rightHeight = getHeight(node.right);
+    if (rightHeight < 0) return rightHeight;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  return getHeight(root) !== -1;
+};
+
+var root = [3, 9, 20, null, null, 15, 7];
+var expected = true;
+var result = isBalanced(createTree(root));
+console.log(result, expected);
+
+var root = [1, 2, 2, 3, 3, null, null, 4, 4];
+var expected = false;
+var result = isBalanced(createTree(root));
+console.log(result, expected);
+
+var root = [];
+var expected = true;
+var result = isBalanced(createTree(root));
+console.log(result, expected);
