299d29ee494193f843f9f869567530a749cfee63
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-10 21:15:09 +0700

    feat(linked-list): :sparkles: add 83. Remove Duplicates from Sorted List - linked-list

---
+++ 0001-0100/081-090/083E_Remove_Duplicates_from_Sorted_List/app.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+// 83. Remove Duplicates from Sorted List
+const { ListNode, createList, toArray } = require('../../../_utils/list');
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let node = head;
+  while (node.next) {
+    if (node.val === node.next.val) {
+      const next = node.next.next;
+      node.next = next;
+    } else {
+      node = node.next;
+    }
+  }
+  return head;
+};
+
+var head = [1, 1, 2];
+var expected = [1, 2];
+var result = deleteDuplicates(createList(head));
+console.log(toArray(result), expected);
+
+var head = [1, 1, 2, 3, 3];
+var expected = [1, 2, 3];
+var result = deleteDuplicates(createList(head));
+console.log(toArray(result), expected);
