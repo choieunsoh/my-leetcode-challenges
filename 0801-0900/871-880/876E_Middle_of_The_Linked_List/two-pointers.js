3f15480d3dedd4c296c1588834b48197e35768f7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-17 10:43:48 +0700

    feat(linked-list): ✨ add 876. Middle of The Linked List - linked list, two pointers

---
+++ 0801-0900/871-880/876E_Middle_of_The_Linked_List/two-pointers.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+// 876. Middle of The Linked List
+const { LinkedList, createList, toArray } = require('../../../_utils/list');
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
+
+var head = createList([1, 2, 3, 4, 5]);
+var expected = [3, 4, 5];
+var result = toArray(middleNode(head));
+console.log(result, result.join() === expected.join());
+
+var head = createList([1, 2, 3, 4, 5, 6]);
+var expected = [4, 5, 6];
+var result = toArray(middleNode(head));
+console.log(result, result.join() === expected.join());
