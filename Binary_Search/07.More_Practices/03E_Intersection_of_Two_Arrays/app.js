e59169bf658f303b581f239c61d80934aec82620
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-23 12:00:45 +0700

    feat(binary-search): ✨ add 349. Intersection of Two Arrays

---
+++ Binary_Search/07.More_Practices/03E_Intersection_of_Two_Arrays/app.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/intersection-of-two-arrays/
+// 349. Intersection of Two Arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  const result = new Set();
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums1.includes(nums2[i])) {
+      result.add(nums2[i]);
+    }
+  }
+  return [...result];
+};
+
+var nums1 = [1, 2, 2, 1],
+  nums2 = [2, 2];
+var expected = [2];
+var result = intersection(nums1, nums2);
+console.log(result.join(' '), result.join('') === expected.join(''));
+
+var nums1 = [4, 9, 5],
+  nums2 = [9, 4, 9, 8, 4];
+var expected = [9, 4];
+var result = intersection(nums1, nums2);
+console.log(result.join(' '), result.join('') === expected.join(''));
