adba2f8bfcb9bd9c0df72a9b78fd2f4dc16c203d
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-30 18:39:29 +0700

    feat(array): ✨ add 228. Summary Ranges

---
+++ 0201-0300/221-230/228E_Summary_Ranges/app.js
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/summary-ranges/
+// 228. Summary Ranges
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  if (nums.length === 0) return [];
+  let result = [];
+  let start = nums[0];
+  let end = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === end + 1) {
+      end = nums[i];
+    } else {
+      if (start === end) {
+        result.push(`${start}`);
+      } else {
+        result.push(`${start}->${end}`);
+      }
+      start = nums[i];
+      end = nums[i];
+    }
+  }
+  if (start === end) {
+    result.push(`${start}`);
+  } else {
+    result.push(`${start}->${end}`);
+  }
+  return result;
+};
+
+var nums = [0, 1, 2, 4, 5, 7];
+var expected = ['0->2', '4->5', '7'];
+var result = summaryRanges(nums);
+console.log(result, result.join() === expected.join());
+
+var nums = [0, 2, 3, 4, 6, 8, 9];
+var expected = ['0', '2->4', '6', '8->9'];
+var result = summaryRanges(nums);
+console.log(result, result.join() === expected.join());
---
+++ 0201-0300/221-230/228E_Summary_Ranges/loop.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/summary-ranges/
+// 228. Summary Ranges
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  if (nums.length === 0) return [];
+  let result = [];
+  let index = 0;
+  while (index < nums.length) {
+    let start = nums[index];
+    while (index + 1 < nums.length && nums[index] + 1 === nums[index + 1]) {
+      index++;
+    }
+    if (start === nums[index]) {
+      result.push(`${start}`);
+    } else {
+      result.push(`${start}->${nums[index]}`);
+    }
+  }
+  return result;
+};
+
+var nums = [0, 1, 2, 4, 5, 7];
+var expected = ['0->2', '4->5', '7'];
+var result = summaryRanges(nums);
+console.log(result, result.join() === expected.join());
+
+var nums = [0, 2, 3, 4, 6, 8, 9];
+var expected = ['0', '2->4', '6', '8->9'];
+var result = summaryRanges(nums);
+console.log(result, result.join() === expected.join());
