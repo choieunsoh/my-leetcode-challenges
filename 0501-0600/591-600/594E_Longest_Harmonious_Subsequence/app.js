5e9981f3f5dc34b600d88789d4e17528a498a8c1
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-07-31 17:34:08 +0700

    feat(array): ✨ add 594. Longest Harmonious Subsequence - hash table

---
+++ 0501-0600/591-600/594E_Longest_Harmonious_Subsequence/app.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/longest-harmonious-subsequence/
+// 594. Longest Harmonious Subsequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+  let result = 0;
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], map.get(nums[i]) + 1 || 1);
+  }
+  for (const num of map.keys()) {
+    if (map.has(num + 1)) {
+      result = Math.max(result, map.get(num) + map.get(num + 1));
+    }
+  }
+  return result;
+};
+
+var nums = [1, 3, 2, 2, 5, 2, 3, 7];
+var expected = 5;
+var result = findLHS(nums);
+console.log(result, result === expected);
+
+var nums = [1, 2, 3, 4];
+var expected = 2;
+var result = findLHS(nums);
+console.log(result, result === expected);
+
+var nums = [1, 1, 1, 1];
+var expected = 0;
+var result = findLHS(nums);
+console.log(result, result === expected);
