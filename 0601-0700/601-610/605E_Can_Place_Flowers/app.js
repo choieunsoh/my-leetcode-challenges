cf03d10e674d4792bdb2e17d0601117e369b25ae
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-08-01 11:56:42 +0700

    feat(array): ✨ add 605. Can Place Flowers - greedy

---
+++ 0601-0700/601-610/605E_Can_Place_Flowers/app.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/can-place-flowers/
+// 605. Can Place Flowers
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; n > 0 && i < flowerbed.length; i++) {
+    const left = flowerbed[i - 1] ?? 0;
+    const right = flowerbed[i + 1] ?? 0;
+    if (flowerbed[i] + left + right === 0) {
+      flowerbed[i] = 1;
+      n--;
+    }
+  }
+  return n === 0;
+};
+
+var flowerbed = [1, 0, 0, 0, 1],
+  n = 1;
+var expected = true;
+var actual = canPlaceFlowers(flowerbed, n);
+console.log(actual, actual === expected);
+
+var flowerbed = [1, 0, 0, 0, 1],
+  n = 2;
+var expected = false;
+var actual = canPlaceFlowers(flowerbed, n);
+console.log(actual, actual === expected);
