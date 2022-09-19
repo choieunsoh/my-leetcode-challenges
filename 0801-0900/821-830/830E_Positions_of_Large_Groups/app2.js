d20823021de6904f89fc6e2942c257af1bcdbb71
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-09-19 23:29:52 +0700

    feat(strings): ✨ add 830. Positions of Large Groups - new solution

---
+++ 0801-0900/821-830/830E_Positions_of_Large_Groups/app2.js
@@ -0,0 +1,51 @@
+// https://leetcode.com/problems/positions-of-large-groups/
+// 830. Positions of Large Groups
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function (s) {
+  const result = [];
+  let start = 0;
+  let end = 0;
+
+  while (end < s.length) {
+    if (s[start] !== s[end]) {
+      end - start >= 3 && result.push([start, end - 1]);
+      start = end;
+    }
+    end++;
+  }
+  end - start >= 3 && result.push([start, end - 1]);
+
+  return result;
+};
+
+var s = 'abbxxxxzzy';
+var expected = [[3, 6]];
+var result = largeGroupPositions(s);
+console.log(result, result.join() === expected.join());
+
+var s = 'abc';
+var expected = [];
+var result = largeGroupPositions(s);
+console.log(result, result.join() === expected.join());
+
+var s = 'aa';
+var expected = [];
+var result = largeGroupPositions(s);
+console.log(result, result.join() === expected.join());
+
+var s = 'aaa';
+var expected = [[0, 2]];
+var result = largeGroupPositions(s);
+console.log(result, result.join() === expected.join());
+
+var s = 'abcdddeeeeaabbbcd';
+var expected = [
+  [3, 5],
+  [6, 9],
+  [12, 14],
+];
+var result = largeGroupPositions(s);
+console.log(result, result.join() === expected.join());
