e567997893faec599d4a35daf942b609aab0d327
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-14 17:49:38 +0700

    feat(array): ✨ add 442. Find All Duplicates in an Array - TS, JS - negative marking

---
+++ 0401-0500/441-450/442M_Find_All_Duplicates_in_an_Array/app.js
@@ -0,0 +1,33 @@
+// 442. Find All Duplicates in an Array
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      result.push(index + 1);
+    } else {
+      nums[index] *= -1;
+    }
+  }
+  return result;
+};
+
+var nums = [4, 3, 2, 7, 8, 2, 3, 1];
+var expected = [2, 3];
+var result = findDuplicates(nums);
+console.log(result, result.join() === expected.join());
+
+var nums = [1, 1, 2];
+var expected = [1];
+var result = findDuplicates(nums);
+console.log(result, result.join() === expected.join());
+
+var nums = [1];
+var expected = [];
+var result = findDuplicates(nums);
+console.log(result, result.join() === expected.join());
