0220df0aa04779f9f5a397bde851da1c1e60e1f3
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-16 10:21:12 +0700

    feat(array): ✨ add 713. Subarray Product Less Than K - TS+JS - sliding window

---
+++ 0701-0800/711-720/713M_Subarray_Product_Less_Than_K/app.js
@@ -0,0 +1,34 @@
+// 713. Subarray Product Less Than K
+// https://leetcode.com/problems/subarray-product-less-than-k/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function (nums, k) {
+  if (k <= 1) return 0;
+  let count = 0;
+  let product = 1;
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) {
+    product *= nums[right];
+    while (product >= k) {
+      product /= nums[left++];
+    }
+    count += right - left + 1;
+  }
+
+  return count;
+};
+
+var nums = [10, 5, 2, 6],
+  k = 100;
+var expected = 8;
+var result = numSubarrayProductLessThanK(nums, k);
+console.log(result, expected, result === expected);
+
+var nums = [1, 2, 3],
+  k = 0;
+var expected = 0;
+var result = numSubarrayProductLessThanK(nums, k);
+console.log(result, expected, result === expected);
