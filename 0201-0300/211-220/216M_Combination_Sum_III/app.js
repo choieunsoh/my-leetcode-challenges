8b936e63881aef45bd459612bad31061830becc8
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-24 00:52:52 +0700

    feat(backtracking): ✨ add 216. Combination Sum III - TS+JS

---
+++ 0201-0300/211-220/216M_Combination_Sum_III/app.js
@@ -0,0 +1,50 @@
+// 216. Combination Sum III
+// https://leetcode.com/problems/combination-sum-iii/
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number[][]}
+ */
+var combinationSum3 = function (k, n) {
+  const result = [];
+  combine(1, 0, []);
+  return result;
+
+  function combine(start, sum, nums) {
+    if (sum > n) return;
+    if (nums.length === k) {
+      if (sum === n) result.push([...nums]);
+      return;
+    }
+
+    for (let i = start; i <= 9; i++) {
+      if (sum + i > n) break;
+
+      nums.push(i);
+      combine(i + 1, sum + i, nums);
+      nums.pop();
+    }
+  }
+};
+
+var k = 3,
+  n = 7;
+var expected = [[1, 2, 4]];
+var result = combinationSum3(k, n);
+console.log(result, result.sort().join() === expected.sort().join());
+
+var k = 3,
+  n = 9;
+var expected = [
+  [1, 2, 6],
+  [1, 3, 5],
+  [2, 3, 4],
+];
+var result = combinationSum3(k, n);
+console.log(result, result.sort().join() === expected.sort().join());
+
+var k = 4,
+  n = 1;
+var expected = [];
+var result = combinationSum3(k, n);
+console.log(result, result.sort().join() === expected.sort().join());
