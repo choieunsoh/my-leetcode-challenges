45603fcf9234723e6d35b47c6a8c23552d8ede8b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-26 21:32:22 +0700

    feat(bfs+dfs): ✨ add 572. Subtree of Another Tree - JS - dfs

---
+++ 0501-0600/571-580/572E_Subtree_of_Another_Tree/app.js
@@ -0,0 +1,49 @@
+// 572. Subtree of Another Tree
+// https://leetcode.com/problems/subtree-of-another-tree/
+const { TreeNode, createTree, levelOrder } = require('../../../_utils/tree');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  if (!root || !subRoot) return false;
+  if (isSameTree(root, subRoot)) return true;
+
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+  function isSameTree(root1, root2) {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    if (root1.val !== root2.val) return false;
+    return (
+      isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right)
+    );
+  }
+};
+
+var root = [3, 4, 5, 1, 2],
+  subRoot = [4, 1, 2];
+var expected = true;
+var result = isSubtree(createTree(root), createTree(subRoot));
+console.log(result, result === expected);
+
+var root = [3, 4, 5, 1, 2, null, null, null, null, 0],
+  subRoot = [4, 1, 2];
+var expected = false;
+var result = isSubtree(createTree(root), createTree(subRoot));
+console.log(result, result === expected);
+
+var root = [1, 1],
+  subRoot = [1];
+var expected = true;
+var result = isSubtree(createTree(root), createTree(subRoot));
+console.log(result, result === expected);
