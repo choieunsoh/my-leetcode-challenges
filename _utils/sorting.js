3f40802363af9c47837f4616f30df52e7f677ba0
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-30 21:04:15 +0700

    feat(sorting-searching): ✨ add quick sort

--- _utils/sorting.js
+++ _utils/sorting.js
@@ -20,7 +20,33 @@ var mergeSort = function (nums) {
   }
 };
 
+var quickSort = function (nums, left = 0, right = nums.length - 1) {
+  if (left >= right) return;
+  const pivotIndex = partition(nums, left, right);
+  quickSort(nums, left, pivotIndex - 1);
+  quickSort(nums, pivotIndex + 1, right);
+  return nums;
+
+  function partition(nums, left, right) {
+    const pivot = nums[right];
+    let i = left;
+    for (let j = left; j <= right; j++) {
+      if (nums[j] < pivot) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+        i++;
+      }
+    }
+    [nums[i], nums[right]] = [nums[right], nums[i]];
+    return i;
+  }
+};
+
 var nums = [2, 5, 3, 1, 4];
 var expected = [1, 2, 3, 4, 5];
 var result = mergeSort(nums);
+console.log(result, result.join() === expected.join());
+
+var nums = [2, 5, 3, 1, 4];
+var expected = [1, 2, 3, 4, 5];
+var result = quickSort(nums);
 console.log(result, result.join() === expected.join());
