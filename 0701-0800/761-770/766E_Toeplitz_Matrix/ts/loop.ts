7dd7049696f95e8d2aa76e3ce47bc2a36ac0cb44
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-31 07:40:49 +0700

    feat(array): ✨ add 766. Toeplitz Matrix - TS

---
+++ 0701-0800/761-770/766E_Toeplitz_Matrix/ts/loop.ts
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/toeplitz-matrix/
+// 766. Toeplitz Matrix
+var isToeplitzMatrix = function (matrix: number[][]): boolean {
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 0; j < matrix[i].length - 1; j++) {
+      if (matrix[i][j] !== matrix[i + 1][j + 1]) return false;
+    }
+  }
+  return true;
+};
+
+var matrix = [
+  [1, 2, 3, 4],
+  [5, 1, 2, 3],
+  [9, 5, 1, 2],
+];
+var expected = true;
+var actual = isToeplitzMatrix(matrix);
+console.log(actual, actual === expected);
+
+var matrix = [
+  [1, 2],
+  [2, 2],
+];
+var expected = false;
+var actual = isToeplitzMatrix(matrix);
+console.log(actual, actual === expected);
