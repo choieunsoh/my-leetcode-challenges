fcef52c2c93170536b1dbe4951d7b03d5d0abc31
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-10-31 09:05:42 +0700

    feat(linked-list): ✨ add 876. Middle of The Linked List - tS - fast & slow

---
+++ 0801-0900/871-880/876E_Middle_of_The_Linked_List/ts/fast-slow.ts
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+// 876. Middle of The Linked List
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { ListNode, createList, toArray } from '../../../../_utils/list';
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+var middleNode = function (head: ListNode | null): ListNode | null {
+  let fast = head;
+  let slow = head;
+  while (fast && fast.next && slow) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
+
+var head = createList([1, 2, 3, 4, 5]);
+var expected = [3, 4, 5];
+var result = toArray(middleNode(head));
+console.log(result, result.join() === expected.join());
+
+var head = createList([1, 2, 3, 4, 5, 6]);
+var expected = [4, 5, 6];
+var result = toArray(middleNode(head));
+console.log(result, result.join() === expected.join());
