f71bfab7ad9cb9099c62d9b5e92eced52c6f4cfb
Kiattipong Kamonrat <marsimadz@hotmail.com>
2022-12-27 09:39:56 +0700

    feat(dynamic-programming): ⚡️ update 416. Partition Equal Subset Sum - JS - dp, reduce space

--- 0401-0500/411-420/416M_Partition_Equal_Subset_Sum/dp.js
+++ 0401-0500/411-420/416M_Partition_Equal_Subset_Sum/dp.js
@@ -9,24 +9,19 @@ var canPartition = function (nums) {
   if (total % 2 === 1) return false;
   total /= 2;
 
-  const n = nums.length;
-  let prev = Array(total + 1).fill(false);
-  prev[0] = true;
+  const dp = Array(total + 1).fill(false);
+  dp[0] = true;
 
-  for (let i = 1; i <= n; i++) {
-    const curr = Array(total + 1).fill(false);
-    curr[0] = true;
+  for (let i = 1; i <= nums.length; i++) {
     const num = nums[i - 1];
     for (let sum = 1; sum <= total; sum++) {
-      curr[sum] = prev[sum];
       if (sum >= num) {
-        curr[sum] ||= prev[sum - num];
+        dp[sum] ||= dp[sum - num];
       }
     }
-    prev = curr;
   }
 
-  return prev[total];
+  return dp[total];
 };
 
 var nums = [1, 5, 11, 5];
