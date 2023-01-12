c041054bc80569ef04077732a85e5a3004c74195
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-01-12 09:50:24 +0700

    feat(bfs+dfs): ✨ add 752. Open the Lock - TS - bfs

--- 0701-0800/751-760/752M_Open_the_Lock/ts/bfs.ts
+++ 0701-0800/751-760/752M_Open_the_Lock/ts/bfs.ts
@@ -1,14 +1,9 @@
 // 752. Open the Lock
 // https://leetcode.com/problems/open-the-lock/
-/**
- * @param {string[]} deadends
- * @param {string} target
- * @return {number}
- */
-var openLock = function (deadends, target) {
+var openLock = function (deadends: string[], target: string): number {
   const visited = new Set(deadends);
 
-  function turn(current, pos) {
+  function turn(current: string, pos: number): string[] {
     const next = (Number(current[pos]) + 1) % 10;
     const prev = (Number(current[pos]) + 9) % 10;
 
@@ -18,38 +13,26 @@ var openLock = function (deadends, target) {
     ];
   }
 
-  let count = 0;
   let turns = 0;
-  const queue = [];
+  const queue: string[] = [];
   if (!visited.has('0000')) {
     queue.push('0000');
     visited.add('0000');
   }
 
-  let found = 0;
   while (queue.length > 0) {
     const size = queue.length;
     for (let i = 0; i < size; i++) {
-      count++;
-      let current = queue.shift();
-      //console.log(count, turns, current, found);
+      const current = queue.shift()!;
       if (current === target) return turns;
 
       for (let j = 0; j < 4; j++) {
-        const [next, prev] = turn(current, j);
-        if (!visited.has(next)) {
-          queue.push(next);
-          visited.add(next);
-        } else {
-          found++;
-        }
-
-        if (!visited.has(prev)) {
-          queue.push(prev);
-          visited.add(prev);
-        } else {
-          found++;
-        }
+        const nextTurns = turn(current, j);
+        for (const next of nextTurns)
+          if (!visited.has(next)) {
+            queue.push(next);
+            visited.add(next);
+          }
       }
     }
     turns++;
@@ -61,19 +44,23 @@ var openLock = function (deadends, target) {
 var deadends = ['0201', '0101', '0102', '1212', '2002'],
   target = '0202';
 var expected = 6;
-console.log(openLock(deadends, target), expected);
+var result = openLock(deadends, target);
+console.log(result, result === expected);
 
 var deadends = ['8888'],
   target = '0009';
 var expected = 1;
-console.log(openLock(deadends, target), expected);
+var result = openLock(deadends, target);
+console.log(result, result === expected);
 
 var deadends = ['8887', '8889', '8878', '8898', '8788', '8988', '7888', '9888'],
   target = '8888';
 var expected = -1;
-console.log(openLock(deadends, target), expected);
+var result = openLock(deadends, target);
+console.log(result, result === expected);
 
 var deadends = ['0000'],
   target = '8888';
 var expected = -1;
-console.log(openLock(deadends, target), expected);
+var result = openLock(deadends, target);
+console.log(result, result === expected);
