f48c17b3a35ea86233926ebfb119d0b0ba4cf85b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-14 23:14:30 +0700

    feat(array): ✨ add 1480. Running Sum of 1d Array - JS, TS - prefix sum

--- 1401-1500/1471-1480/1480E_Running_Sum_of_1d_Array/ts/app.ts
+++ 1401-1500/1471-1480/1480E_Running_Sum_of_1d_Array/ts/app.ts
@@ -1,10 +1,6 @@
 // 1480. Running Sum of 1d Array
 // https://leetcode.com/problems/running-sum-of-1d-array/
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var runningSum = function (nums) {
+var runningSum = function (nums: number[]): number[] {
   for (let i = 1; i < nums.length; i++) {
     nums[i] += nums[i - 1];
   }
