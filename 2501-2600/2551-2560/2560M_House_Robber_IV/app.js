8a5d25ad5bdd2fb2cf97ec52b476260f16024f89
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-14 09:54:32 +0700

    feat(binary-search): ✨ add 2560. House Robber IV - JS

---
+++ 2501-2600/2551-2560/2560M_House_Robber_IV/app.js
@@ -0,0 +1,43 @@
+// 2560. House Robber IV
+// https://leetcode.com/problems/house-robber-iv/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minCapability = function (nums, k) {
+  let left = Math.min(...nums);
+  let right = Math.max(...nums);
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    if (valid(mid, k)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  function valid(limit, remain) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] <= limit) {
+        if (--remain === 0) return true;
+        i++;
+      }
+    }
+    return false;
+  }
+
+  return left;
+};
+
+var nums = [2, 3, 5, 9],
+  k = 2;
+var expected = 5;
+var result = minCapability(nums, k);
+console.log(result, result === expected);
+
+var nums = [2, 7, 9, 3, 1],
+  k = 2;
+var expected = 2;
+var result = minCapability(nums, k);
+console.log(result, result === expected);
