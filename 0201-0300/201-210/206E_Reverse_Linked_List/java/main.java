6f1b37831ca6c6cb9d9cafe21c13b6fd6e45e374
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-16 13:37:06 +0700

    feat(linked-list): ✨ add 206. Reverse Linked List - Java

---
+++ 0201-0300/201-210/206E_Reverse_Linked_List/java/main.java
@@ -0,0 +1,32 @@
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    while (curr != null) {
+      ListNode next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  }
+}
