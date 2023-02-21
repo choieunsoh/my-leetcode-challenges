270d9f637ccf1d8a8d68f6e4fec92921417466b7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-21 17:29:01 +0700

    feat(hash-table): ✨ add 496. Next Greater Element I - TS

---
+++ 0401-0500/491-500/496E_Next_Greater_Element_I/ts/app.ts
@@ -0,0 +1,42 @@
+// 496. Next Greater Element I
+// https://leetcode.com/problems/next-greater-element-i/
+var nextGreaterElement = function (nums1: number[], nums2: number[]): number[] {
+  const result: number[] = [];
+  const next = new Map<number, number>();
+  const stack: number[] = [];
+  for (let i = 0; i < nums2.length; i++) {
+    const current = nums2[i];
+    while (stack.length > 0 && current > stack[stack.length - 1]) {
+      next.set(stack.pop()!, current);
+    }
+    stack.push(current);
+  }
+
+  for (let i = 0; i < nums1.length; i++) {
+    const num = nums1[i];
+    if (next.has(num)) {
+      result.push(next.get(num)!);
+    } else {
+      result.push(-1);
+    }
+  }
+  return result;
+};
+
+var nums1 = [4, 1, 2],
+  nums2 = [1, 3, 4, 2];
+var expected = [-1, 3, -1];
+var result = nextGreaterElement(nums1, nums2);
+console.log(result, expected.join() === result.join());
+
+var nums1 = [2, 4],
+  nums2 = [1, 2, 3, 4];
+var expected = [3, -1];
+var result = nextGreaterElement(nums1, nums2);
+console.log(result, expected.join() === result.join());
+
+var nums1 = [1, 3, 5, 2, 4],
+  nums2 = [6, 5, 4, 3, 2, 1, 7];
+var expected = [7, 7, 7, 7, 7];
+var result = nextGreaterElement(nums1, nums2);
+console.log(result, expected.join() === result.join());
