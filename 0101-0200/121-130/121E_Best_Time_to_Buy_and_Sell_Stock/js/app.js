7a3f6c9c8b13a595f350f77fa808269db0563e8b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-22 08:44:01 +0700

    feat(greedy): ✨ add 121. Best Time to Buy and Sell Stock - JS - kadane's algo

---
+++ 0101-0200/121-130/121E_Best_Time_to_Buy_and_Sell_Stock/js/app.js
@@ -0,0 +1,26 @@
+// 121. Best Time to Buy and Sell Stock
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let profit = 0;
+  let buy = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
+  }
+
+  return profit;
+};
+
+var prices = [7, 1, 5, 3, 6, 4];
+var expect = 5;
+var result = maxProfit(prices);
+console.log(result, expect, result === expect);
+
+var prices = [7, 6, 4, 3, 1];
+var expect = 0;
+var result = maxProfit(prices);
+console.log(result, expect, result === expect);
