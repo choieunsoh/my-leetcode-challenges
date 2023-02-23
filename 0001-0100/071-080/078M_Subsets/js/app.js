02b8a6c061e474b376834a7b9aa36c313d0bcd9c
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-23 11:38:44 +0700

    feat(backtracking): ✨ add 78. Subsets - JS

---
+++ 0001-0100/071-080/078M_Subsets/js/app.js
@@ -0,0 +1,30 @@
+// 78. Subsets
+// https://leetcode.com/problems/subsets/
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  const result = [];
+  backtrack(0, []);
+  return result;
+
+  function backtrack(start, path) {
+    result.push(path.slice());
+    for (let i = start; i < nums.length; i++) {
+      path.push(nums[i]);
+      backtrack(i + 1, path.slice());
+      path.pop();
+    }
+  }
+};
+
+var nums = [1, 2, 3];
+var expected = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]];
+var result = subsets(nums);
+console.log(result, result.sort().join() === expected.sort().join());
+
+var nums = [0];
+var expected = [[], [0]];
+var result = subsets(nums);
+console.log(result, result.sort().join() === expected.sort().join());
