166340bbe7111e6d35fe8fadb2f31b064e41f264
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-02-24 14:29:14 +0700

    feat(bfs+dfs): ✨ add 127. Word Ladder - JS - bfs

---
+++ 0101-0200/121-130/127H_Word_Ladder/app.js
@@ -0,0 +1,50 @@
+// 127. Word Ladder
+// https://leetcode.com/problems/word-ladder/
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, wordList) {
+  const words = new Set(wordList);
+  if (!words.has(endWord)) return 0;
+
+  let changes = 1;
+  const a = 'a'.charCodeAt(0);
+  const seen = new Set();
+  const queue = [beginWord];
+  while (queue.length) {
+    const size = queue.length;
+    for (let k = 0; k < size; k++) {
+      const word = queue.shift();
+      if (word === endWord) return changes;
+      for (let i = 0; i < word.length; i++) {
+        for (let j = 0; j < 26; j++) {
+          const ch = String.fromCharCode(a + j);
+          const nextWord = word.substring(0, i) + ch + word.substring(i + 1);
+          if (words.has(nextWord) && !seen.has(nextWord)) {
+            seen.add(nextWord);
+            queue.push(nextWord);
+          }
+        }
+      }
+    }
+    changes++;
+  }
+  return 0;
+};
+
+var beginWord = 'hit',
+  endWord = 'cog',
+  wordList = ['hot', 'dot', 'dog', 'lot', 'log', 'cog'];
+var expected = 5;
+var result = ladderLength(beginWord, endWord, wordList);
+console.log(result, result === expected);
+
+var beginWord = 'hit',
+  endWord = 'cog',
+  wordList = ['hot', 'dot', 'dog', 'lot', 'log'];
+var expected = 0;
+var result = ladderLength(beginWord, endWord, wordList);
+console.log(result, result === expected);
