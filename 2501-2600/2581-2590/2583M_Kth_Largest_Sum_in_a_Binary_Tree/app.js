0c17f3e29d7d99e40e601e5e49e5e22be435e9fe
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-05 19:22:43 +0700

    feat(heap): ✨ add 2583. Kth Largest Sum in a Binary Tree - JS - bfs, stack, max pq

---
+++ 2501-2600/2581-2590/2583M_Kth_Largest_Sum_in_a_Binary_Tree/app.js
@@ -0,0 +1,58 @@
+// 2583. Kth Largest Sum in a Binary Tree
+// https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree/
+const { TreeNode, createTree, inOrder } = require('../../../_utils/tree');
+const { MaxPriorityQueue } = require('@datastructures-js/priority-queue');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestLevelSum = function (root, k) {
+  const pq = new MaxPriorityQueue();
+  let q = [root];
+  while (q.length) {
+    const newQ = [];
+    let sum = 0;
+    for (let i = 0; i < q.length; i++) {
+      const node = q[i];
+      sum += node.val;
+      if (node.left) newQ.push(node.left);
+      if (node.right) newQ.push(node.right);
+    }
+    q = newQ;
+    pq.enqueue(sum);
+  }
+
+  let result = -1;
+  if (pq.size() < k) return result;
+  for (let i = 0; i < k; i++) {
+    result = pq.dequeue();
+  }
+  return result;
+};
+
+var root = createTree([5, 8, 9, 2, 1, 3, 7, 4, 6]),
+  k = 2;
+var expected = 13;
+var result = kthLargestLevelSum(root, k);
+console.log(result, result === expected);
+
+var root = createTree([1, 2, null, 3]),
+  k = 1;
+var expected = 3;
+var result = kthLargestLevelSum(root, k);
+console.log(result, result === expected);
+
+var root = createTree([1, 2, null, 3]),
+  k = 9;
+var expected = -1;
+var result = kthLargestLevelSum(root, k);
+console.log(result, result === expected);
