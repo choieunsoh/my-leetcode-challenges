f8c0bbfc3170ef5efda03f6e043e3c640064d40d
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-26 13:53:31 +0700

    feat(linked-list): ✨ add removeKFromList - CodeSignal - JS

---
+++ CodeSignal/Linked_List/001E_removeKFromList/app.js
@@ -0,0 +1,24 @@
+// removeKFromList
+// LC-203: https://leetcode.com/problems/remove-linked-list-elements/
+// Singly-linked lists are already defined with this interface:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+function removeKFromList(l, k) {
+  if (!l) return null;
+  let temp = new ListNode(0);
+  temp.next = l;
+  let prev = temp;
+  let curr = temp.next;
+  while (curr) {
+    if (curr.value === k) {
+      prev.next = curr.next;
+    } else {
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return temp.next;
+}
