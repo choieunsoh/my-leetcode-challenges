201b0180e624f2aea08607113c40f67b4b2dc550
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-26 13:31:07 +0700

    feat(hash-table): ✨ add containsCloseNums - CodeSignal - JS

---
+++ CodeSignal/Hash_Table/003.containsCloseNums/app.js
@@ -0,0 +1,30 @@
+// containsCloseNums
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+function solution(nums, k) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (map.has(num)) {
+      const j = map.get(num);
+      if (i - j <= k) return true;
+    }
+    map.set(num, i);
+  }
+  return false;
+}
+
+var nums = [0, 1, 2, 3, 5, 2],
+  k = 3;
+var expected = true;
+var result = solution(nums, k);
+console.log(result, result === expected);
+
+var nums = [0, 1, 2, 3, 5, 2],
+  k = 2;
+var expected = false;
+var result = solution(nums, k);
+console.log(result, result === expected);
