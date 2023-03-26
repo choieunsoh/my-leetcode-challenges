33fd9f0533da71365cd10aeb3c508d76564ff9b8
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-26 13:58:55 +0700

    feat(linked-list): ✨ add mergeTwoLinkedLists - JS

---
+++ CodeSignal/Linked_List/004M_mergeTwoLinkedLists/app.js
@@ -0,0 +1,24 @@
+// mergeTwoLinkedLists
+// LC-21: https://leetcode.com/problems/merge-two-sorted-lists/
+// Singly-linked lists are already defined with this interface:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function mergeTwoLinkedLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.value < l2.value) {
+    l1.next = solution(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = solution(l1, l2.next);
+    return l2;
+  }
+}
