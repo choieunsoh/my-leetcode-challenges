b04317a1be405fd9b8f13f67711b618c9cca7574
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-26 15:52:08 +0700

    feat(math): ✨ add 2523. Closest Prime Numbers in Range - JS

---
+++ 2501-2600/2521-2530/2523M_Closest_Prime_Numbers_in_Range/app.js
@@ -0,0 +1,60 @@
+// 2523. Closest Prime Numbers in Range
+// https://leetcode.com/problems/closest-prime-numbers-in-range/
+let prime = [];
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var closestPrimes = function (left, right) {
+  const result = [-1, -1];
+  if (prime.length === 0) {
+    prime = createPrimeNumber(1e6 + 1);
+  }
+
+  let min = Number.MAX_SAFE_INTEGER;
+  let [one, two] = result;
+  for (let i = left; i <= right; i++) {
+    if (!prime[i]) continue;
+    [one, two] = [two, i];
+    if (one !== -1 && two !== -1) {
+      if (two - one < min) {
+        min = two - one;
+        result[0] = one;
+        result[1] = two;
+      }
+    }
+  }
+  return result;
+
+  function createPrimeNumber(n) {
+    const prime = Array(n + 1).fill(true);
+    prime[0] = false;
+    prime[1] = false;
+    for (let i = 2; i * i < n; i++) {
+      if (!prime[i]) continue;
+      for (let j = i * i; j < n; j += i) {
+        prime[j] = false;
+      }
+    }
+    return prime;
+  }
+};
+
+var left = 10,
+  right = 19;
+var expected = [11, 13];
+var result = closestPrimes(left, right);
+console.log(result, result.join() === expected.join());
+
+var left = 4,
+  right = 6;
+var expected = [-1, -1];
+var result = closestPrimes(left, right);
+console.log(result, result.join() === expected.join());
+
+var left = 147106,
+  right = 213773;
+var expected = [147137, 147139];
+var result = closestPrimes(left, right);
+console.log(result, result.join() === expected.join());
