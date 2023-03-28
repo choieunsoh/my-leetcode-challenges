044b9fdecc1e80b5f554e9eba2847d2cb6d5dc02
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-03-28 09:55:17 +0700

    feat(hash-table): ✨ add 1346. Check If N and Its Double Exist - JS

---
+++ 1301-1400/1341-1350/1346E_Check_If_N_and_Its_Double_Exist/app2.js
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/check-if-n-and-its-double-exist/
+// 1346. Check If N and Its Double Exist
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+  const double = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    double.set(num * 2, i);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    const index = double.get(num) ?? -1;
+    if (index !== -1 && index !== i) return true;
+  }
+  return false;
+};
+
+var arr = [10, 2, 5, 3];
+var expected = true;
+var result = checkIfExist(arr);
+console.log(result, result === expected);
+
+var arr = [7, 1, 14, 11];
+var expected = true;
+var result = checkIfExist(arr);
+console.log(result, result === expected);
+
+var arr = [3, 1, 7, 11];
+var expected = false;
+var result = checkIfExist(arr);
+console.log(result, result === expected);
+
+var arr = [0, 0];
+var expected = true;
+var result = checkIfExist(arr);
+console.log(result, result === expected);
+
+var arr = [0, 1];
+var expected = false;
+var result = checkIfExist(arr);
+console.log(result, result === expected);
