20dbc323afa9f72bb68583487e5680411893ee05
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-05 16:33:18 +0700

    feat(hash-table): ✨ [top150] add 219. Contains Duplicate II - JS

---
+++ Top_Interview_150/05.Hashmap/046E_Contains_Duplicate_II/app.js
@@ -0,0 +1,38 @@
+// 219. Contains Duplicate II
+// https://leetcode.com/problems/contains-duplicate-ii/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const seen = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (seen.has(num)) {
+      const j = seen.get(num);
+      if (Math.abs(i - j) <= k) return true;
+    }
+
+    seen.set(num, i);
+  }
+  return false;
+};
+
+var nums = [1, 2, 3, 1],
+  k = 3;
+var expected = true;
+var result = containsNearbyDuplicate(nums, k);
+console.log(result, result === expected);
+
+var nums = [1, 0, 1, 1],
+  k = 1;
+var expected = true;
+var result = containsNearbyDuplicate(nums, k);
+console.log(result, result === expected);
+
+var nums = [1, 2, 3, 1, 2, 3],
+  k = 2;
+var expected = false;
+var result = containsNearbyDuplicate(nums, k);
+console.log(result, result === expected);
