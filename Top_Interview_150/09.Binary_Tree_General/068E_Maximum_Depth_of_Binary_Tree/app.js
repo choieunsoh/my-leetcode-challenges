f639d91b52f2682ed8d7776cf87cb0229e4bfbd5
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-06 18:14:16 +0700

    feat(trees): ✨ [top150] add 104. Maximum Depth of Binary Tree - JS

---
+++ Top_Interview_150/09.Binary_Tree_General/068E_Maximum_Depth_of_Binary_Tree/app.js
@@ -0,0 +1,32 @@
+// 104. Maximum Depth of Binary Tree
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+const { TreeNode, createTree, printTree } = require('../../../_utils/tree');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root === null) return 0;
+
+  const depthLeft = maxDepth(root.left);
+  const depthRight = maxDepth(root.right);
+  return Math.max(depthLeft, depthRight) + 1;
+};
+
+var root = createTree([3, 9, 20, null, null, 15, 7]);
+var expect = 3;
+var depth = maxDepth(root);
+console.log(depth, depth === expect);
+
+var root = createTree([1, null, 2]);
+var expect = 2;
+var depth = maxDepth(root);
+console.log(depth, depth === expect);
