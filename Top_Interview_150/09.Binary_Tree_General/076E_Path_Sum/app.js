53395cf8c3c2ce8dc54e91152d32e1669842bf5b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-06 19:33:31 +0700

    feat(trees): ✨ [top150] add 112. Path Sum - JS

---
+++ Top_Interview_150/09.Binary_Tree_General/076E_Path_Sum/app.js
@@ -0,0 +1,49 @@
+// 112. Path Sum
+// https://leetcode.com/problems/path-sum/
+const { TreeNode, createTree } = require('../../../_utils/tree');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+  if (root === null) return false;
+  if (targetSum === root.val && root.left === null && root.right === null) return true;
+
+  const left = hasPathSum(root.left, targetSum - root.val);
+  const right = hasPathSum(root.right, targetSum - root.val);
+
+  return left || right;
+};
+
+var root = createTree([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1]),
+  targetSum = 22;
+var expected = true;
+var result = hasPathSum(root, targetSum);
+console.log(result, result === expected);
+
+var root = createTree([1, 2, 3]),
+  targetSum = 5;
+var expected = false;
+var result = hasPathSum(root, targetSum);
+console.log(result, result === expected);
+
+var root = createTree([]),
+  targetSum = 0;
+var expected = false;
+var result = hasPathSum(root, targetSum);
+console.log(result, result === expected);
+
+var root = createTree([1, 2]),
+  targetSum = 1;
+var expected = false;
+var result = hasPathSum(root, targetSum);
+console.log(result, result === expected);
