1e0bc62a730624492b085a91c4b4020ae45e3946
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-07 19:15:40 +0700

    feat(graph): ✨ [top150] add 133. Clone Graph - JS - bfs

---
+++ Top_Interview_150/12.Graph_General/091M_Clone_Graph/app.js
@@ -0,0 +1,40 @@
+// 133. Clone Graph
+// https://leetcode.com/problems/clone-graph/
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+function Node(val, neighbors) {
+  this.val = val === undefined ? 0 : val;
+  this.neighbors = neighbors === undefined ? [] : neighbors;
+}
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {
+  if (!node) return null;
+  const map = new Map();
+  map.set(node.val, new Node(node.val));
+  let queue = [node];
+  while (queue.length) {
+    const newQueue = [];
+    for (let i = 0; i < queue.length; i++) {
+      const current = queue[i];
+      if (!current.neighbors) continue;
+      for (const neighbor of current.neighbors) {
+        if (!map.has(neighbor.val)) {
+          map.set(neighbor.val, new Node(neighbor.val));
+          newQueue.push(neighbor);
+        }
+        map.get(current.val).neighbors.push(map.get(neighbor.val));
+      }
+    }
+    queue = newQueue;
+  }
+
+  return map.get(node.val);
+};
