da49349efbfa23d7d6864502b99496f58aa532e2
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-07 20:42:34 +0700

    feat(graph): ✨ [top150] add 207. Course Schedule - JS

---
+++ Top_Interview_150/12.Graph_General/093M_Course_Schedule/app.js
@@ -0,0 +1,62 @@
+// 207. Course Schedule
+// https://leetcode.com/problems/course-schedule/
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+  const visited = {};
+  const courses = {};
+  for (const [courseId, preCourse] of prerequisites) {
+    if (!courses[courseId]) {
+      courses[courseId] = [];
+    }
+    courses[courseId].push(preCourse);
+  }
+
+  for (let courseId = 0; courseId < numCourses; courseId++) {
+    if (isCycling(courseId)) return false;
+  }
+  return true;
+
+  function isCycling(courseId) {
+    if (visited[courseId]) return true;
+    if (visited[courseId] === false) return false;
+
+    visited[courseId] = true;
+    const preCourses = courses[courseId] ?? [];
+    for (const preCourse of preCourses) {
+      if (isCycling(preCourse)) return true;
+    }
+    visited[courseId] = false;
+
+    return false;
+  }
+};
+
+var numCourses = 2,
+  prerequisites = [[1, 0]];
+var expected = true;
+var result = canFinish(numCourses, prerequisites);
+console.log(result, result === expected);
+
+var numCourses = 2,
+  prerequisites = [
+    [1, 0],
+    [0, 1],
+  ];
+var expected = false;
+var result = canFinish(numCourses, prerequisites);
+console.log(result, result === expected);
+
+var numCourses = 5,
+  prerequisites = [
+    [1, 4],
+    [2, 4],
+    [3, 1],
+    [3, 2],
+  ];
+var expected = true;
+var result = canFinish(numCourses, prerequisites);
+console.log(result, result === expected);
