1485b057883c3378c1d170b4b62ee8dc2ba0dcc0
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-11 22:52:36 +0700

    feat(backtracking): ✨ [top150] add 39. Combination Sum - JS

---
+++ Top_Interview_150/15.Backtracking/104M_Combination_Sum/app.js
@@ -0,0 +1,47 @@
+// 39. Combination Sum
+// https://leetcode.com/problems/combination-sum/
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+  const result = [];
+  backtracking(0, 0, []);
+  return result;
+
+  function backtracking(start, sum, nums) {
+    if (sum > target) return;
+    if (sum === target) {
+      result.push([...nums]);
+      return;
+    }
+
+    for (let i = start; i < candidates.length; i++) {
+      const num = candidates[i];
+      backtracking(i, sum + num, [...nums, num]);
+    }
+  }
+};
+
+var candidates = [2, 3, 6, 7],
+  target = 7;
+var expected = [[2, 2, 3], [7]];
+var result = combinationSum(candidates, target);
+console.log(result, result.sort().join() === expected.sort().join());
+
+var candidates = [2, 3, 5],
+  target = 8;
+var expected = [
+  [2, 2, 2, 2],
+  [2, 3, 3],
+  [3, 5],
+];
+var result = combinationSum(candidates, target);
+console.log(result, result.sort().join() === expected.sort().join());
+
+var candidates = [2],
+  target = 1;
+var expected = [];
+var result = combinationSum(candidates, target);
+console.log(result, result.sort().join() === expected.sort().join());
