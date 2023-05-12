d6ba51249afa4f31b3dc669bb58eec8e6492e0bd
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-12 09:03:58 +0700

    feat(dynamic-programming): ✨ [top150] add 53. Maximum Subarray - JS

---
+++ Top_Interview_150/17.Kadane's_Algorithm/112M_Maximum_Subarray/app.js
@@ -0,0 +1,36 @@
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let max = nums[0];
+  let sum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    sum = Math.max(sum + num, num);
+    max = Math.max(max, sum);
+  }
+  return max;
+};
+
+var nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+var expected = 6;
+var result = maxSubArray(nums);
+console.log(result, expected, result === expected);
+
+var nums = [1];
+var expected = 1;
+var result = maxSubArray(nums);
+console.log(result, expected, result === expected);
+
+var nums = [5, 4, -1, 7, 8];
+var expected = 23;
+var result = maxSubArray(nums);
+console.log(result, expected, result === expected);
+
+var nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+var expected = 6;
+var result = maxSubArray(nums);
+console.log(result, expected, result === expected);
