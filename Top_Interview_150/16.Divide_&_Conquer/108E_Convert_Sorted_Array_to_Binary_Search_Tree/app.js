cea427051cd045c2ec9dedcbcc6c542c153b8958
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-12 07:04:48 +0700

    feat(trees): ✨ [top150] add 108. Convert Sorted Array to Binary Search Tree - JS

---
+++ Top_Interview_150/16.Divide_&_Conquer/108E_Convert_Sorted_Array_to_Binary_Search_Tree/app.js
@@ -0,0 +1,36 @@
+// 108. Convert Sorted Array to Binary Search Tree
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+const { TreeNode, printTree } = require('../../../_utils/tree');
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums, left = 0, right = nums.length - 1) {
+  if (left > right) return null;
+
+  const mid = (left + right) >> 1;
+  const node = new TreeNode(nums[mid]);
+  node.left = sortedArrayToBST(nums, left, mid - 1);
+  node.right = sortedArrayToBST(nums, mid + 1, right);
+  return node;
+};
+
+var nums = [-10, -3, 0, 5, 9];
+var expect = [0, -3, 9, -10, null, 5];
+var result = sortedArrayToBST(nums);
+console.log(printTree(result));
+console.log(expect);
+
+var nums = [1, 3];
+var expect = [3, 1];
+var result = sortedArrayToBST(nums);
+console.log(printTree(result));
+console.log(expect);
