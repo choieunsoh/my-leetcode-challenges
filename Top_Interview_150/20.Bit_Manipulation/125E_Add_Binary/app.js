629a2de66282dc6a6a7ea3b0c0ccdce741b1ad9f
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-13 22:12:09 +0700

    feat(bit-manipulation): ✨ [top150] add 67. Add Binary - JS

---
+++ Top_Interview_150/20.Bit_Manipulation/125E_Add_Binary/app.js
@@ -0,0 +1,34 @@
+// 67. Add Binary
+// https://leetcode.com/problems/add-binary/
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  let result = '';
+  while (carry || i >= 0 || j >= 0) {
+    const num1 = +a.charAt(i--) ?? 0;
+    const num2 = +b.charAt(j--) ?? 0;
+    const sum = carry + num1 + num2;
+    carry = sum >> 1;
+    result = (sum & 1) + result;
+  }
+
+  return result;
+};
+
+var a = '11',
+  b = '1';
+var expected = '100';
+var result = addBinary(a, b);
+console.log(result, expected === result);
+
+var a = '1010',
+  b = '1011';
+var expected = '10101';
+var result = addBinary(a, b);
+console.log(result, expected === result);
