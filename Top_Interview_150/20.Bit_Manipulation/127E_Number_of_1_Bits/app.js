80cb2899204ee7e39a92b496f0b3f7b43fd57867
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-13 22:21:31 +0700

    feat(bit-manipulation): ✨ [top150] add 191. Number of 1 Bits - JS

---
+++ Top_Interview_150/20.Bit_Manipulation/127E_Number_of_1_Bits/app.js
@@ -0,0 +1,29 @@
+// 191. Number of 1 Bits
+// https://leetcode.com/problems/number-of-1-bits/
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let count = 0;
+  while (n) {
+    n = n & (n - 1);
+    count++;
+  }
+  return count;
+};
+
+var n = Number.parseInt('00000000000000000000000000001011', 2);
+var expected = 3;
+var result = hammingWeight(n);
+console.log(result, result === expected);
+
+var n = Number.parseInt('00000000000000000000000010000000', 2);
+var expected = 1;
+var result = hammingWeight(n);
+console.log(result, result === expected);
+
+var n = Number.parseInt('11111111111111111111111111111101', 2);
+var expected = 31;
+var result = hammingWeight(n);
+console.log(result, result === expected);
