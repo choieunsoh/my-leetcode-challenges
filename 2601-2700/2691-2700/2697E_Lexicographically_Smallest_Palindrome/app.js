b863dbeb1eac99687f4305902b09eb38038aaa10
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-21 13:25:55 +0700

    feat(two-pointers): ✨ add 2697. Lexicographically Smallest Palindrome - JS

---
+++ 2601-2700/2691-2700/2697E_Lexicographically_Smallest_Palindrome/app.js
@@ -0,0 +1,38 @@
+// 2697. Lexicographically Smallest Palindrome
+// https://leetcode.com/problems/lexicographically-smallest-palindrome/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeSmallestPalindrome = function (s) {
+  s = s.split('');
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    if (s[l] === s[r]) {
+      //
+    } else if (s[l] < s[r]) {
+      s[r] = s[l];
+    } else {
+      s[l] = s[r];
+    }
+    l++;
+    r--;
+  }
+  return s.join('');
+};
+
+var s = 'egcfe';
+var expected = 'efcfe';
+var result = makeSmallestPalindrome(s);
+console.log(result, result === expected);
+
+var s = 'abcd';
+var expected = 'abba';
+var result = makeSmallestPalindrome(s);
+console.log(result, result === expected);
+
+var s = 'seven';
+var expected = 'neven';
+var result = makeSmallestPalindrome(s);
+console.log(result, result === expected);
