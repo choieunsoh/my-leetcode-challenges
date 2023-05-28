e4692b6e38396a01548b4370fdbf8bbc064ee08e
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-28 12:45:49 +0700

    feat(greedy): ✨ update 2712. Minimum Cost to Make All Characters Equal - JS

--- 2701-2800/2711-2720/2712M_Minimum_Cost_to_Make_All_Characters_Equal/app+.js
+++ 2701-2800/2711-2720/2712M_Minimum_Cost_to_Make_All_Characters_Equal/app+.js
@@ -5,32 +5,14 @@
  * @return {number}
  */
 var minimumCost = function (s) {
-  if (s.length === 1) return 0;
-  const zero = calc(s, '0');
-  const one = calc(s, '1');
-  return Math.min(one, zero);
-
-  function calc(s, num) {
-    let cost = 0;
-    const n = s.length;
-    const half = (n / 2) | 0;
-    let check = num;
-    for (let i = half - 1; i > -1; i--) {
-      if (s[i] === check) {
-        cost += i + 1;
-        check = check === '0' ? '1' : '0';
-      }
+  let cost = 0;
+  const n = s.length;
+  for (let i = 1; i < n; i++) {
+    if (s[i] !== s[i - 1]) {
+      cost += Math.min(i, n - i);
     }
-
-    check = num;
-    for (let i = half; i < n; i++) {
-      if (s[i] === check) {
-        cost += n - i;
-        check = check === '0' ? '1' : '0';
-      }
-    }
-    return cost;
   }
+  return cost;
 };
 
 var s = '0011';
