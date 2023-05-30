7348515f7d9f0ce14d2ef2301365e1c0ee1c2b8c
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-05-30 08:16:30 +0700

    feat(javascript): ✨ add 2693. Call Function with Custom Context - JS - call, apply, bind

---
+++ 2601-2700/2691-2700/2693M_Call_Function_with_Custom_Context/bind.js
@@ -0,0 +1,31 @@
+// 2693. Call Function with Custom Context
+// https://leetcode.com/problems/call-function-with-custom-context/
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function (context, ...args) {
+  return this.bind(context)(...args);
+};
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
+
+var fn = function add(b) {
+    return this.a + b;
+  },
+  args = [{ a: 5 }, 7];
+var expected = 12;
+var result = fn.callPolyfill(...args);
+console.log(result, result === expected);
+
+var fn = function tax(price, taxRate) {
+    return `The cost of the ${this.item} is ${price * taxRate}`;
+  },
+  args = [{ item: 'burger' }, 10, 1.1];
+var expected = 'The cost of the burger is 11';
+var result = fn.callPolyfill(...args);
+console.log(result, result === expected);
