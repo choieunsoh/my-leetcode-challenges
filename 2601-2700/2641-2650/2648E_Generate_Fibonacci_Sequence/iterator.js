71ebf9cf1f5a55e37bab7d35a6c746357f04311b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-06-02 07:59:33 +0700

    feat(javascript): ✨ add 2648. Generate Fibonacci Sequence - JS - system.iterator, generator

---
+++ 2601-2700/2641-2650/2648E_Generate_Fibonacci_Sequence/iterator.js
@@ -0,0 +1,47 @@
+// 2648. Generate Fibonacci Sequence
+// https://leetcode.com/problems/generate-fibonacci-sequence/
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function () {
+  // Determine the number of Fibonacci numbers to generate
+  const lengthOfSequence = 50;
+  const fibonacciSequence = Array(lengthOfSequence).fill(0);
+  fibonacciSequence[1] = 1;
+
+  for (let i = 2; i < fibonacciSequence.length; i++) {
+    fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+  }
+
+  // Return an iterator for the array
+  return fibonacciSequence[Symbol.iterator]();
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
+
+var gen = fibGenerator();
+var callCount = 5;
+var outputs = [0, 1, 1, 2, 3];
+for (let i = 0; i < callCount; i++) {
+  const expected = outputs[i];
+  const result = gen.next().value;
+  console.log(result, result === expected);
+}
+
+var gen = fibGenerator();
+var callCount = 50;
+var outputs = [
+  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368,
+  75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817,
+  39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073,
+  4807526976, 7778742049,
+];
+for (let i = 0; i < callCount; i++) {
+  const expected = outputs[i];
+  const result = gen.next().value;
+  console.log(result, result === expected);
+}
