858cf6cb7271f24a0841fcdb014d4583545b69d3
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-06-03 07:36:32 +0700

    feat(javascript): ✨ add 2649. Nested Array Generator - JS

---
+++ 2601-2700/2641-2650/2649M_Nested_Array_Generator/flat.js
@@ -0,0 +1,35 @@
+// 2649. Nested Array Generator
+// https://leetcode.com/problems/nested-array-generator/
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+  const nums = arr.flat(Infinity);
+  for (const num of nums) {
+    yield num;
+  }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
+
+var arr = [1, [2, 3]];
+var outputs = [1, 2, 3];
+var gen = inorderTraversal(arr);
+for (const expected of outputs) {
+  const result = gen.next().value;
+  console.log(result, result === expected);
+}
+
+var arr = [[[6]], [1, 3], []];
+var outputs = [6, 1, 3];
+var gen = inorderTraversal(arr);
+for (const expected of outputs) {
+  const result = gen.next().value;
+  console.log(result, result === expected);
+}
