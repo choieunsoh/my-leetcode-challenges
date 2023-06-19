6703fcec94040ab30cc2683640f28a13adfc2b8c
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-06-19 07:10:54 +0700

    feat(prefix-sum): ✨ add 1732. Find the Highest Altitude - JS

--- 1701-1800/1731-1740/1732E_Find_the_Highest_Altitude/app.js
+++ 1701-1800/1731-1740/1732E_Find_the_Highest_Altitude/app.js
@@ -1,17 +1,17 @@
-// https://leetcode.com/problems/find-the-highest-altitude/
 // 1732. Find the Highest Altitude
+// https://leetcode.com/problems/find-the-highest-altitude/
 /**
  * @param {number[]} gain
  * @return {number}
  */
 var largestAltitude = function (gain) {
-  let highest = Number.MIN_SAFE_INTEGER;
+  let highest = 0;
   let altitude = 0;
   for (let i = 0; i < gain.length; i++) {
     altitude += gain[i];
     if (altitude > highest) highest = altitude;
   }
-  return Math.max(0, highest);
+  return highest;
 };
 
 var gain = [-5, 1, 5, 0, -7];
