17a8a856cf96d0321761f9a890278e8ac629b8ad
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-03 08:20:37 +0700

    feat(javascript): ✨ add 2704. To Be Or Not To Be - JS

---
+++ 2701-2800/2701-2710/2704E_To_Be_Or_Not_To_Be/app.js
@@ -0,0 +1,23 @@
+// 2704. To Be Or Not To Be
+// https://leetcode.com/problems/to-be-or-not-to-be
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: (val2) => {
+      if (val !== val2) throw new Error('Not Equal');
+      else return true;
+    },
+    notToBe: (val2) => {
+      if (val === val2) throw new Error('Equal');
+      else return true;
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
