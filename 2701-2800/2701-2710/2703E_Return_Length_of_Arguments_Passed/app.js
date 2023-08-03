0caf94d65030d2a871f4a7a0ea1aebc89ffc70e0
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-03 08:27:53 +0700

    feat(javascript): ✨ add 2703. Return Length of Arguments Passed - JS

---
+++ 2701-2800/2701-2710/2703E_Return_Length_of_Arguments_Passed/app.js
@@ -0,0 +1,12 @@
+// 2703. Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed
+/**
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return arguments.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
