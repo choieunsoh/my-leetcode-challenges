6e174e9ca1193b6120fa79fd0ebf91177df7d634
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-05 08:01:20 +0700

    feat(javascript): ✨ update 2619. Array Prototype Last - JS

--- 2601-2700/2611-2620/2619E_Array_Prototype_Last/app.js
+++ 2601-2700/2611-2620/2619E_Array_Prototype_Last/app.js
@@ -1,7 +1,8 @@
 // 2619. Array Prototype Last
 // https://leetcode.com/problems/array-prototype-last/
 Array.prototype.last = function () {
-  return this[this.length - 1] ?? -1;
+  if (this.length === 0) return -1;
+  return this[this.length - 1] ?? null;
 };
 
 /**
