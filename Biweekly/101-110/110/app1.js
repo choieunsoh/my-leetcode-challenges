b9cf6c2d8823ec8cf7aab68593a3d578434c65ac
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-06 06:52:16 +0700

    feat(weekly-contest): ✨ add biweekly contest 110

---
+++ Biweekly/101-110/110/app1.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} purchaseAmount
+ * @return {number}
+ */
+var accountBalanceAfterPurchase = function (purchaseAmount) {
+  return 100 - Math.round(purchaseAmount / 10) * 10;
+};
+
+var purchaseAmount = 9;
+var expected = 90;
+var result = accountBalanceAfterPurchase(purchaseAmount);
+console.log(result, result === expected);
+
+var purchaseAmount = 11;
+var expected = 90;
+var result = accountBalanceAfterPurchase(purchaseAmount);
+console.log(result, result === expected);
+
+var purchaseAmount = 15;
+var expected = 80;
+var result = accountBalanceAfterPurchase(purchaseAmount);
+console.log(result, result === expected);
---
+++ Biweekly/101-110/110/app2.js
@@ -0,0 +1,41 @@
+const { ListNode, createList, toArray } = require('../../../_utils/list');
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertGreatestCommonDivisors = function (head) {
+  let temp = new ListNode(0, head);
+  let curr = head;
+  while (curr && curr.next) {
+    const a = curr.val;
+    const c = curr.next.val;
+    const b = gcd(a, c);
+    console.log(a, b, c);
+    const middle = new ListNode(b);
+    middle.next = curr.next;
+    curr.next = middle;
+    curr = middle.next;
+  }
+  return temp.next;
+
+  function gcd(a, b) {
+    return a === 0 ? b : gcd(b % a, a);
+  }
+};
+
+var head = createList([18, 6, 10, 3]);
+var expected = [18, 6, 6, 2, 10, 1, 3];
+var result = toArray(insertGreatestCommonDivisors(head));
+console.log(result, result.join() === expected.join());
+
+var head = createList([7]);
+var expected = [7];
+var result = toArray(insertGreatestCommonDivisors(head));
+console.log(result, result.join() === expected.join());
