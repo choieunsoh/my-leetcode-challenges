80779b3d7fd585073c7eb0f346a3367936576086
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-05 07:35:33 +0700

    feat(dynamic-programming): ✨ add 95. Unique Binary Search Trees II - JS

--- 0001-0100/091-100/095M_Unique_Binary_Search_Trees_II/memo.js
+++ 0001-0100/091-100/095M_Unique_Binary_Search_Trees_II/memo.js
@@ -1,6 +1,6 @@
-// https://leetcode.com/problems/unique-binary-search-trees-ii/
 // 95. Unique Binary Search Trees II
-const { TreeNode } = require('../../../_utils/tree');
+// https://leetcode.com/problems/unique-binary-search-trees-ii/
+const { TreeNode, toLevelOrderArray } = require('../../../_utils/tree');
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -14,9 +14,8 @@ const { TreeNode } = require('../../../_utils/tree');
  * @return {TreeNode[]}
  */
 var generateTrees = function (n) {
-  const cache = Array(n + 1)
-    .fill()
-    .map(() => Array(n + 1).fill());
+  const cache = Array.from({ length: n + 1 }, () => new Array(n + 1));
+  return createTree(1, n);
 
   function createTree(start, end) {
     if (start > end) return [null];
@@ -27,16 +26,38 @@ var generateTrees = function (n) {
         const leftNodes = createTree(start, i - 1);
         const rightNodes = createTree(i + 1, end);
 
-        leftNodes.forEach((leftNode) => {
-          rightNodes.forEach((rightNode) => {
+        for (const leftNode of leftNodes) {
+          for (const rightNode of rightNodes) {
             const node = new TreeNode(i, leftNode, rightNode);
             cache[start][end].push(node);
-          });
-        });
+          }
+        }
       }
     }
     return cache[start][end];
   }
-
-  return createTree(1, n);
 };
+
+var n = 3;
+var expected = [
+  [1, null, 2, null, 3],
+  [1, null, 3, 2],
+  [2, 1, 3],
+  [3, 1, null, null, 2],
+  [3, 2, null, 1],
+];
+var result = generateTrees(n);
+for (var i = 0; i < result.length; i++) {
+  console.log(expected[i]);
+  console.log(toLevelOrderArray(result[i]));
+}
+console.log(JSON.stringify(result.map(toLevelOrderArray)) === JSON.stringify(expected));
+
+var n = 1;
+var expected = [[1]];
+var result = generateTrees(n);
+for (var i = 0; i < result.length; i++) {
+  console.log(expected[i]);
+  console.log(toLevelOrderArray(result[i]));
+}
+console.log(JSON.stringify(result.map(toLevelOrderArray)) === JSON.stringify(expected));
