86ef5ee8e4fc8a7a9008a50095ca62def2eda6f9
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-07 09:44:39 +0700

    feat(pandas): ✨ add 607. Sales Person - py

---
+++ 0601-0700/601-610/607E_Sales_Person/app.py
@@ -0,0 +1,16 @@
+# 607. Sales Person
+# https://leetcode.com/problems/sales-person/
+import pandas as pd
+
+
+def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    return sales_person[
+        ~sales_person['sales_id'].isin(
+            pd.merge(
+                left=orders,
+                right=company[company['name'] == 'RED'],
+                on='com_id',
+                how='inner',
+            )['sales_id'].unique()
+        )
+    ][['name']]
