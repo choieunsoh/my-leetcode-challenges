5025b170663d2ca30acc0b4223bf3f9624487c00
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-08-07 09:41:32 +0700

    feat(pandas): ✨ add 1693. Daily Leads and Partners - py

---
+++ 1601-1700/1691-1700/1693E_Daily_Leads_and_Partners/app.py
@@ -0,0 +1,12 @@
+# 1693. Daily Leads and Partners
+# https://leetcode.com/problems/daily-leads-and-partners/
+import pandas as pd
+
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    return daily_sales.groupby(
+        ['date_id', 'make_name']
+    ).nunique().reset_index().rename(columns={
+        'lead_id': 'unique_leads',
+        'partner_id': 'unique_partners',
+    })
