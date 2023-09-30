80000df534f08605c399d8cce0c84182dbd02e2e
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-09-30 10:05:10 +0700

    fix(stack): 🐛 update 456. 132 Pattern - JS

--- 0401-0500/451-460/456M_132_Pattern/app.js
+++ 0401-0500/451-460/456M_132_Pattern/app.js
@@ -9,24 +9,36 @@
 var find132pattern = function (nums) {
   const n = nums.length;
   if (n < 3) return false;
-  const min = new Array(n);
-  min[0] = nums[0];
+
+  const min_i = new Array(n);
+  min_i[0] = nums[0];
   for (let i = 1; i < n; i++) {
-    min[i] = Math.min(min[i - 1], nums[i]);
+    min_i[i] = Math.min(min_i[i - 1], nums[i]);
   }
 
-  const stack = [];
-  for (let i = n - 1; i >= 0; i--) {
-    if (nums[i] <= min[i]) continue;
-    while (stack.length && stack[stack.length - 1] < min[i]) {
-      stack.pop();
+  // num_i, num_j, num_k
+  // 132 = num_i < num_k < num_j
+  const stack_k = [];
+  for (let j = n - 1; j >= 0; j--) {
+    const num_i = min_i[j];
+    const num_j = nums[j];
+
+    if (num_j <= num_i) continue;
+
+    while (stack_k.length && num_k() <= num_i) {
+      stack_k.pop();
     }
-    if (stack.length && stack[stack.length - 1] < nums[i]) {
+
+    if (stack_k.length && num_k() < num_j) {
       return true;
     }
-    stack.push(nums[i]);
+    stack_k.push(num_j);
   }
   return false;
+
+  function num_k() {
+    return stack_k[stack_k.length - 1];
+  }
 };
 
 var nums = [1, 2, 3, 4];
@@ -45,6 +57,11 @@ var result = find132pattern(nums);
 console.log(result, result === expected);
 
 var nums = [2, 3, 1, 2];
+var expected = false;
+var result = find132pattern(nums);
+console.log(result, result === expected);
+
+var nums = [6, 12, 3, 4, 6, 11, 20];
 var expected = true;
 var result = find132pattern(nums);
 console.log(result, result === expected);
