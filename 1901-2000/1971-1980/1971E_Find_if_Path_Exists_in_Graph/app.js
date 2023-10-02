01ae8458fcf339b717ac40eb81a00c4a035d3489
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-10-02 17:34:32 +0700

    feat(graph): ✨ add 1971. Find if Path Exists in Graph - JS - bfs

---
+++ 1901-2000/1971-1980/1971E_Find_if_Path_Exists_in_Graph/app.js
@@ -0,0 +1,60 @@
+// 1971. Find if Path Exists in Graph
+// https://leetcode.com/problems/find-if-path-exists-in-graph
+// T.C.: O(N)
+// S.C.: O(N)
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function (n, edges, source, destination) {
+  const adjList = Array.from({ length: n }, () => []);
+  for (const [u, v] of edges) {
+    adjList[u].push(v);
+    adjList[v].push(u);
+  }
+
+  const stack = [source];
+  const visited = new Array(n).fill(false);
+  visited[source] = true;
+  while (stack.length) {
+    const current = stack.pop();
+    if (current === destination) return true;
+    for (const neighbor of adjList[current]) {
+      if (!visited[neighbor]) {
+        stack.push(neighbor);
+        visited[neighbor] = true;
+      }
+    }
+  }
+
+  return false;
+};
+
+var n = 3,
+  edges = [
+    [0, 1],
+    [1, 2],
+    [2, 0],
+  ],
+  source = 0,
+  destination = 2;
+var expected = true;
+var result = validPath(n, edges, source, destination);
+console.log(result, result === expected);
+
+var n = 6,
+  edges = [
+    [0, 1],
+    [0, 2],
+    [3, 5],
+    [5, 4],
+    [4, 3],
+  ],
+  source = 0,
+  destination = 5;
+var expected = false;
+var result = validPath(n, edges, source, destination);
+console.log(result, result === expected);
