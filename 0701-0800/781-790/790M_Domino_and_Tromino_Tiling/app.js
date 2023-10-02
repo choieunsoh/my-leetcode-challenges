6fd2f3f39e044c60c5c2f5cd7ca71e2ae252c72b
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-10-02 16:25:12 +0700

    feat(dynamic-programming): ✨ add 790. Domino and Tromino Tiling - JS

---
+++ 0701-0800/781-790/790M_Domino_and_Tromino_Tiling/app.js
@@ -0,0 +1,31 @@
+// 790. Domino and Tromino Tiling
+// https://leetcode.com/problems/domino-and-tromino-tiling/
+// T.C.: O(N)
+// S.C.: O(1)
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTilings = function (n) {
+  const MOD = 1e9 + 7;
+  if (n <= 2) return n;
+
+  const dp = new Array(n).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 5;
+  for (let i = 4; i <= n; i++) {
+    dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+  }
+  return dp[n];
+};
+
+var n = 3;
+var expected = 5;
+var result = numTilings(n);
+console.log(result, result === expected);
+
+var n = 1;
+var expected = 1;
+var result = numTilings(n);
+console.log(result, result === expected);
