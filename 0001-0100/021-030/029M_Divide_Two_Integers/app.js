5cbc48bd974cccfca08dcafd231d951267848b3f
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-10-09 07:52:46 +0700

    fix(math): ✨ update 29. Divide Two Integers - JS

--- 0001-0100/021-030/029M_Divide_Two_Integers/app.js
+++ 0001-0100/021-030/029M_Divide_Two_Integers/app.js
@@ -6,15 +6,12 @@
  * @return {number}
  */
 var divide = function (dividend, divisor) {
+  if (dividend === -2147483648 && divisor === -1) return 2147483647;
+
   const isNegative = dividend > 0 !== divisor > 0;
-  const MAX = 2 ** 31 - 1;
-  const MIN = -(2 ** 31);
   let absDividend = Math.abs(dividend);
   let absDivisor = Math.abs(divisor);
   let result = 0;
-
-  if ((dividend >= MAX || dividend <= MIN) && absDivisor === 1)
-    return isNegative ? MAX : MIN;
 
   while (absDividend >= absDivisor) {
     let temp = absDivisor;
@@ -32,6 +29,20 @@ var divide = function (dividend, divisor) {
   return isNegative ? -result : result;
 };
 
+var divide1 = function (A, B) {
+  if (A === -2147483648 && B === -1) return 2147483647;
+  let ans = 0,
+    sign = 1;
+  if (A < 0) (A = -A), (sign = -sign);
+  if (B < 0) (B = -B), (sign = -sign);
+  if (A === B) return sign;
+  for (let i = 0, val = B; A >= B; i = 0, val = B) {
+    while (val > 0 && val <= A) val = B << ++i;
+    (A -= B << (i - 1)), (ans += 1 << (i - 1));
+  }
+  return sign < 0 ? -ans : ans;
+};
+
 var dividend = 10,
   divisor = 3;
 var expected = 3;
@@ -41,5 +52,11 @@ console.log(result, result === expected);
 var dividend = 7,
   divisor = -3;
 var expected = -2;
+var result = divide(dividend, divisor);
+console.log(result, result === expected);
+
+var dividend = 2147483647,
+  divisor = -1;
+var expected = -2147483647;
 var result = divide(dividend, divisor);
 console.log(result, result === expected);
