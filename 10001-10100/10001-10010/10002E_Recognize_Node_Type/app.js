d8da607258348730dfdb12d597c5957cb4c79307
Kiattipong Kamonrat <marsimadz@hotmail.com>
2023-12-07 08:23:16 +0700

    feat(frontend): ✨ add 10002. Recognize Node Type - JS

---
+++ 10001-10100/10001-10010/10002E_Recognize_Node_Type/app.js
@@ -0,0 +1,21 @@
+// 10002. Recognize Node Type
+// https://leetcode.com/problems/recognize-node-type/
+/**
+ * @param {Node} node
+ * @return {string}
+ */
+export const recognizeNodeType = (node) => {
+  const nodeType = node.nodeType;
+  switch (nodeType) {
+    case Node.ELEMENT_NODE:
+      return 'ELEMENT_NODE';
+    case Node.ATTRIBUTE_NODE:
+      return 'ATTRIBUTE_NODE';
+    case Node.TEXT_NODE:
+      return 'TEXT_NODE';
+    case Node.COMMENT_NODE:
+      return 'COMMENT_NODE';
+    case Node.DOCUMENT_NODE:
+      return 'DOCUMENT_NODE';
+  }
+};
