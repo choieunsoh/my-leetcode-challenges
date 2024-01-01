31ad58d82a68b484ad0c5793922c1737646c87a5
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-01 09:44:44 +0700

    feat(frontend): ✨ add 10004. Simple Query Selector All - JS

---
+++ 10001-10100/10001-10010/10004E_Simple_Query_Selector_All/app.js
@@ -0,0 +1,17 @@
+// 10004. Simple Query Selector All
+// https://leetcode.com/problems/simple-query-selector-all/description/
+/**
+ * @param {string} selector
+ * @return {HTMLElement[]}
+ */
+export const querySelectorAll = (selector) => {
+  const firstChar = selector.charAt(0);
+  const name = selector.substring(1);
+  if (firstChar === '#') {
+    return [document.getElementById(name)];
+  } else if (firstChar === '.') {
+    return document.getElementsByClassName(name);
+  } else {
+    return document.getElementsByTagName(selector);
+  }
+};
