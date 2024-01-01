be4395a03c816504f3dadb9606ed3f234f5a73d8
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-01 10:24:53 +0700

    feat(frontend): ✨ add 10006. Get Elements by Attribute - JS

---
+++ 10001-10100/10001-10010/10006M_Get_Elements_by_Attribute/app.js
@@ -0,0 +1,22 @@
+// 10006. Get Elements by Attribute
+// https://leetcode.com/problems/get-elements-by-attribute/description/
+/**
+ * @param {string} attributeValue
+ * @return {HTMLElement[]}
+ */
+export const getElementsByAttribute = (attributeValue) => {
+  const result = [];
+  const [attribute, value] = attributeValue.split('=');
+  dfs(document.body);
+  return result;
+
+  function dfs(node) {
+    if (node.nodeType === Node.ELEMENT_NODE && node.getAttribute(attribute) === value) {
+      result.push(node);
+    }
+
+    for (const childNode of node.childNodes) {
+      dfs(childNode);
+    }
+  }
+};
