891928b70e6d0100b944619803c9d9e8bb22ced7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-01 09:36:45 +0700

    feat(frontend): ✨ add 10003. Apply Styles to Node - JS

---
+++ 10001-10100/10001-10010/10003E_Apply_Styles_to_Node/app.js
@@ -0,0 +1,12 @@
+// 10003. Apply Styles to Node
+// https://leetcode.com/problems/apply-styles-to-node/description/
+/**
+ * @param {HTMLElement} node
+ * @return {void}
+ */
+export const applyStylesToNode = (node) => {
+  node.style.backgroundColor = '#ffa015';
+  node.style.color = 'white';
+  node.style.fontSize = '16px';
+  node.style.borderStyle = 'none';
+};
