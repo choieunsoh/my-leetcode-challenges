9b8a09396b281631e7ad47dfb4d994281d1a4370
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-02 07:12:54 +0700

    feat(hash-table): ✨ add 2610. Convert an Array Into a 2D Array With Conditions - JS

--- 2601-2700/2601-2610/2610M_Convert_an_Array_Into_a_2D_Array_With_Conditions/app.js
+++ 2601-2700/2601-2610/2610M_Convert_an_Array_Into_a_2D_Array_With_Conditions/app.js
@@ -1,4 +1,7 @@
 // 2610. Convert an Array Into a 2D Array With Conditions
+// https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/description/
+// T.C.: O(n)
+// S.C.: O(n)
 /**
  * @param {number[]} nums
  * @return {number[][]}
