665d613f65aeaeef2cc995c663e0dd450b07cc23
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-02 07:04:33 +0700

    feat(frontend): ✨ add 10007. Get Node Relationship - JS

---
+++ 10001-10100/10001-10010/10007E_Get_Node_Relationship/app.js
@@ -0,0 +1,14 @@
+// 10007. Get Node Relationship
+// https://leetcode.com/problems/get-node-relationship/description/
+/**
+ * @param {Node} node1
+ * @param {Node} node2
+ * @return {"parent"|"child"|"sibling"|null}
+ */
+export const getNodeRelationship = (node1, node2) => {
+  if (node1 === node2) return null;
+  if (node1.parentNode === node2) return 'parent';
+  if (node2.parentNode === node1) return 'child';
+  if (node1.parentNode === node2.parentNode) return 'sibling';
+  return null;
+};
