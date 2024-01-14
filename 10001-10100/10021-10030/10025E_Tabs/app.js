ffd2e559628a8939f24db38276856d1ad8fbd1a7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-01-14 07:42:28 +0700

    feat(react): ✨ add 10025. Tabs - JS

---
+++ 10001-10100/10021-10030/10025E_Tabs/app.js
@@ -0,0 +1,51 @@
+// 10025. Tabs
+// https://leetcode.com/problems/tabs/description/
+
+import React from 'react';
+
+/**
+ * @typedef {Object} TabObject
+ * @property {string} label
+ * @property {string} content
+ */
+
+/**
+ * @param {Object} props
+ * @param {TabObject[]} props.tabsList
+ * @return {JSX.Element}
+ */
+export const Tabs = ({ tabsList }) => {
+  const [activeTab, setActiveTab] = React.useState(0);
+
+  const handleClick = (event) => {
+    const target = event.target;
+    if (target.dataset.index !== undefined) {
+      setActiveTab(Number(target.dataset.index));
+    }
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <div>
+        {tabsList.map((tab, index) => (
+          <div
+            key={index}
+            data-index={index}
+            style={{
+              padding: '10px',
+              marginRight: '10px',
+              cursor: 'pointer',
+              backgroundColor: index === activeTab ? 'lightgray' : 'transparent',
+              border: '1px solid gray',
+              fontWeight: index === activeTab ? 'bold' : 'normal',
+            }}
+          >
+            {tab.label}
+          </div>
+        ))}
+      </div>
+
+      <div>{tabsList[activeTab].content}</div>
+    </div>
+  );
+};
