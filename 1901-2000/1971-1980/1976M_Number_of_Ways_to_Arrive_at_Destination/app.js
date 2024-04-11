d3f5a9a53fa2094be8df63a6367b8f02e4bdbcd7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-04-11 08:17:26 +0700

    ✨ feat(graph): add 1976. Number of Ways to Arrive at Destination - JS

---
+++ 1901-2000/1971-1980/1976M_Number_of_Ways_to_Arrive_at_Destination/app.js
@@ -0,0 +1,67 @@
+// 1976. Number of Ways to Arrive at Destination
+// https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/description/
+// T.C.: O(n log n)
+// S.C.: O(n + m)
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var countPaths = function (n, roads) {
+  const graph = Array.from({ length: n }, () => []);
+  for (const [u, v, w] of roads) {
+    graph[u].push([v, w]);
+    graph[v].push([u, w]);
+  }
+
+  const distance = new Array(n).fill(Infinity);
+  distance[n - 1] = 0;
+  const ways = new Array(n).fill(0);
+  ways[n - 1] = 1;
+  dijkstra();
+  return ways[0];
+
+  function dijkstra() {
+    const MOD = 1e9 + 7;
+    const pq = new MinPriorityQueue();
+    pq.enqueue([0, n - 1], 0);
+    while (!pq.isEmpty()) {
+      const [dist, currNode] = pq.dequeue();
+      if (dist > distance[currNode]) continue;
+      for (const [nextNode, nextDist] of graph[currNode]) {
+        const totalDist = dist + nextDist;
+        if (totalDist < distance[nextNode]) {
+          distance[nextNode] = totalDist;
+          pq.enqueue([totalDist, nextNode], totalDist);
+          ways[nextNode] = ways[currNode];
+        } else if (totalDist === distance[nextNode]) {
+          ways[nextNode] = (ways[nextNode] + ways[currNode]) % MOD;
+        }
+      }
+    }
+  }
+};
+
+var n = 7,
+  roads = [
+    [0, 6, 7],
+    [0, 1, 2],
+    [1, 2, 3],
+    [1, 3, 3],
+    [6, 3, 3],
+    [3, 5, 1],
+    [6, 5, 1],
+    [2, 5, 1],
+    [0, 4, 5],
+    [4, 6, 2],
+  ];
+var expected = 4;
+var result = countPaths(n, roads);
+console.log(result, result === expected);
+
+var n = 2,
+  roads = [[1, 0, 10]];
+var expected = 1;
+var result = countPaths(n, roads);
+console.log(result, result === expected);
