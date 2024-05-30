ebb08e39b64ee742d43388edefcb96cd8379974d
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-05-30 09:43:19 +0700

    ✨ feat(prefix-sum): add 1442. Count Triplets That Can Form Two Arrays of Equal XOR - JS

---
+++ 1401-1500/1441-1450/1442M_Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR/app2.js
@@ -0,0 +1,39 @@
+// 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+// https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/description/
+// T.C.: O(n)
+// S.C.: O(n)
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countTriplets = function (arr) {
+  let result = 0;
+  const prefixXOR = [0, ...arr];
+  const n = prefixXOR.length;
+  for (let i = 1; i < n; i++) {
+    prefixXOR[i] ^= prefixXOR[i - 1];
+  }
+  const countMap = new Map();
+  const totalMap = new Map();
+  for (let i = 0; i < n; i++) {
+    const prefix = prefixXOR[i];
+    const count = countMap.get(prefix) ?? 0;
+    const total = totalMap.get(prefix) ?? 0;
+    result += count * (i - 1) - total;
+
+    countMap.set(prefix, count + 1);
+    totalMap.set(prefix, total + i);
+  }
+
+  return result;
+};
+
+var arr = [2, 3, 1, 6, 7];
+var expected = 4;
+var result = countTriplets(arr);
+console.log(result, result === expected);
+
+var arr = [1, 1, 1, 1, 1];
+var expected = 10;
+var result = countTriplets(arr);
+console.log(result, result === expected);
