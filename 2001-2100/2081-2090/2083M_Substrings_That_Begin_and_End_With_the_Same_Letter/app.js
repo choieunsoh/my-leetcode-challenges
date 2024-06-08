e9531980710447832de2d0b0cfec4f9ba99fc349
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-06-08 09:44:20 +0700

    ✨ feat(counting): add 2083. Substrings That Begin and End With the Same Letter - JS

---
+++ 2001-2100/2081-2090/2083M_Substrings_That_Begin_and_End_With_the_Same_Letter/app.js
@@ -0,0 +1,34 @@
+// 2083. Substrings That Begin and End With the Same Letter
+// https://leetcode.com/problems/substrings-that-begin-and-end-with-the-same-letter/description/
+// T.C.: O(n)
+// S.C.: O(1)
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function (s) {
+  let result = 0;
+  const a = 'a'.charCodeAt(0);
+  const counts = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    const index = s.charCodeAt(i) - a;
+    counts[index]++;
+    result += counts[index];
+  }
+  return result;
+};
+
+var s = 'abcba';
+var expected = 7;
+var result = numberOfSubstrings(s);
+console.log(result, result === expected);
+
+var s = 'abacad';
+var expected = 9;
+var result = numberOfSubstrings(s);
+console.log(result, result === expected);
+
+var s = 'a';
+var expected = 1;
+var result = numberOfSubstrings(s);
+console.log(result, result === expected);
