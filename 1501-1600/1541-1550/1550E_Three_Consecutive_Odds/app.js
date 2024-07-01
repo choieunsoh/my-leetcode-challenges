70fc03dea2db54daf2fc369c1471ab081b81e95f
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-01 07:44:35 +0700

    ✨ feat(array): add 1550. Three Consecutive Odds - JS

---
+++ 1501-1600/1541-1550/1550E_Three_Consecutive_Odds/app.js
@@ -0,0 +1,26 @@
+// 1550. Three Consecutive Odds
+// https://leetcode.com/problems/three-consecutive-odds/description/
+// T.C.: O(n)
+// S.C.: O(1)
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] % 2 === 1 && arr[i - 1] % 2 === 1 && arr[i - 2] % 2 === 1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var arr = [2, 6, 4, 1];
+var expected = false;
+var result = threeConsecutiveOdds(arr);
+console.log(result, result === expected);
+
+var arr = [1, 2, 34, 3, 4, 5, 7, 23, 12];
+var expected = true;
+var result = threeConsecutiveOdds(arr);
+console.log(result, result === expected);
