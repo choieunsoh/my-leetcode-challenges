01dc0b9242144115e495004d7c398ce72a8010b7
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-08 08:52:16 +0700

    🐛  fix(array): update S.C. of 1823. Find the Winner of the Circular Game - JS

--- 1801-1900/1821-1830/1823M_Find_the_Winner_of_the_Circular_Game/app.js
+++ 1801-1900/1821-1830/1823M_Find_the_Winner_of_the_Circular_Game/app.js
@@ -1,7 +1,7 @@
 // 1823. Find the Winner of the Circular Game
 // https://leetcode.com/problems/find-the-winner-of-the-circular-game/
 // T.C.: O(n)
-// S.C.: O(n)
+// S.C.: O(1)
 /**
  * @param {number} n
  * @param {number} k
