d99a9fd2edecd1bbbb1570088e6d23478fde9f11
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-07-24 08:18:03 +0700

    ✨ feat(sorting): add 2191. Sort the Jumbled Numbers - JS

---
+++ 2101-2200/2191-2200/2191M_Sort_the_Jumbled_Numbers/app2.js
@@ -0,0 +1,50 @@
+// 2191. Sort the Jumbled Numbers
+// https://leetcode.com/problems/sort-the-jumbled-numbers/description/
+// T.C.: O(n log n)
+// S.C.: O(n)
+/**
+ * @param {number[]} mapping
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortJumbled = function (mapping, nums) {
+  nums.sort((a, b) => {
+    const mappedA = mappedValue(a);
+    const mappedB = mappedValue(b);
+    if (mappedA === mappedB) {
+      return 0;
+    }
+    return mappedA - mappedB;
+  });
+  return nums;
+
+  function mappedValue(num) {
+    if (num < 10) return mapping[num];
+    let value = 0;
+    let base = 1;
+    while (num) {
+      value += base * mapping[num % 10];
+      num = Math.floor(num / 10);
+      base *= 10;
+    }
+    return value;
+  }
+};
+
+var mapping = [8, 9, 4, 0, 2, 1, 3, 5, 7, 6],
+  nums = [991, 338, 38];
+var expected = [338, 38, 991];
+var result = sortJumbled(mapping, nums);
+console.log(result, result.join() === expected.join());
+
+var mapping = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  nums = [789, 456, 123];
+var expected = [123, 456, 789];
+var result = sortJumbled(mapping, nums);
+console.log(result, result.join() === expected.join());
+
+var mapping = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+  nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var expected = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+var result = sortJumbled(mapping, nums);
+console.log(result, result.join() === expected.join());
