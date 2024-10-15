a88646f422024e9e5363a1736526c86fb6e7aeab
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-10-15 07:58:39 +0700

    ✨ feat(two-pointer): add 2938. Separate Black and White Balls - JS

---
+++ 2901-3000/2931-2940/2938M_Separate_Black_and_White_Balls/app.js
@@ -0,0 +1,44 @@
+// 2938. Separate Black and White Balls
+// https://leetcode.com/problems/separate-black-and-white-balls/description/
+// T.C.: O(n)
+// S.C.: O(1)
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumSteps = function (s) {
+  const n = s.length;
+  let swaps = 0;
+  let left = 0;
+  for (let right = 0; right < n; right++) {
+    if (s.charAt(right) === '1') continue;
+    swaps += right - left;
+    left++;
+  }
+  return swaps;
+};
+
+var s = '101';
+var expected = 1;
+var result = minimumSteps(s);
+console.log(result, result === expected);
+
+var s = '100';
+var expected = 2;
+var result = minimumSteps(s);
+console.log(result, result === expected);
+
+var s = '0111';
+var expected = 0;
+var result = minimumSteps(s);
+console.log(result, result === expected);
+
+var s = '101110';
+var expected = 5;
+var result = minimumSteps(s);
+console.log(result, result === expected);
+
+var s = '101010';
+var expected = 6;
+var result = minimumSteps(s);
+console.log(result, result === expected);
