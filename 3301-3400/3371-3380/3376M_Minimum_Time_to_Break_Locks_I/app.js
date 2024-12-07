e7e94b8f061c550d8993043447669336963b046c
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-12-07 23:34:02 +0700

    ✨ feat(dynamic-programming): add 3376. Minimum Time to Break Locks I - JS

---
+++ 3301-3400/3371-3380/3376M_Minimum_Time_to_Break_Locks_I/app.js
@@ -0,0 +1,58 @@
+// 3376. Minimum Time to Break Locks I
+// https://leetcode.com/problems/minimum-time-to-break-locks-i/description/
+// T.C.: O(2^n)
+// S.C.: O(2^n)
+/**
+ * @param {number[]} strength
+ * @param {number} K
+ * @return {number}
+ */
+var findMinimumTime = function (strength, K) {
+  const n = strength.length;
+  const memo = new Map();
+  return dfs((1 << n) - 1, 1);
+
+  function dfs(currentLocks, currentX) {
+    if (currentLocks === 0) return 0;
+
+    const key = `${currentLocks},${currentX}`;
+    if (memo.has(key)) return memo.get(key);
+
+    let minTime = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < n; i++) {
+      if ((currentLocks & (1 << i)) === 0) continue;
+
+      const lockTime = Math.ceil(strength[i] / currentX);
+      const newLocks = currentLocks & ~(1 << i);
+      const totalTime = lockTime + dfs(newLocks, currentX + K);
+      minTime = Math.min(minTime, totalTime);
+    }
+
+    memo.set(key, minTime);
+    return minTime;
+  }
+};
+
+var strength = [3, 4, 1],
+  K = 1;
+var expected = 4;
+var result = findMinimumTime(strength, K);
+console.log(result, result === expected);
+
+var strength = [2, 5, 4],
+  K = 2;
+var expected = 5;
+var result = findMinimumTime(strength, K);
+console.log(result, result === expected);
+
+var strength = [40, 50],
+  K = 4;
+var expected = 50;
+var result = findMinimumTime(strength, K);
+console.log(result, result === expected);
+
+var strength = [7, 3, 6, 18, 22, 50],
+  K = 4;
+var expected = 12;
+var result = findMinimumTime(strength, K);
+console.log(result, result === expected);
