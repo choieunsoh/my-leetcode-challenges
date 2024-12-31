e75c408554ab218908bad148c99a8c0491ae73b6
Kiattipong Kamonrat <marsimadz@hotmail.com>
2024-12-31 09:08:15 +0700

    ✨ feat(dynamic-programming): update 983. Minimum Cost For Tickets - JS

--- 0901-1000/981-990/983M_Minimum_Cost_For_Tickets/app.js
+++ 0901-1000/981-990/983M_Minimum_Cost_For_Tickets/app.js
@@ -1,5 +1,7 @@
 // 983. Minimum Cost For Tickets
 // https://leetcode.com/problems/minimum-cost-for-tickets/
+// T.C: O(n)
+// S.C: O(n)
 /**
  * @param {number[]} days
  * @param {number[]} costs
@@ -7,18 +9,19 @@
  */
 var mincostTickets = function (days, costs) {
   const seen = new Set(days);
-  const dp = Array(366).fill(-1);
+  const memo = new Array(366).fill(-1);
   return dfs(1);
 
   function dfs(day) {
     if (day > 365) return 0;
-    if (dp[day] !== -1) return dp[day];
+    if (memo[day] !== -1) return memo[day];
     if (!seen.has(day)) return dfs(day + 1);
+
     const dayPassCost = dfs(day + 1) + costs[0];
     const weekPassCost = dfs(day + 7) + costs[1];
     const monthPassCost = dfs(day + 30) + costs[2];
-    dp[day] = Math.min(dayPassCost, weekPassCost, monthPassCost);
-    return dp[day];
+    memo[day] = Math.min(dayPassCost, weekPassCost, monthPassCost);
+    return memo[day];
   }
 };
 
--- 0901-1000/981-990/983M_Minimum_Cost_For_Tickets/dp.js
+++ 0901-1000/981-990/983M_Minimum_Cost_For_Tickets/dp.js
@@ -1,26 +1,28 @@
 // 983. Minimum Cost For Tickets
 // https://leetcode.com/problems/minimum-cost-for-tickets/
+// T.C: O(n)
+// S.C: O(n)
 /**
  * @param {number[]} days
  * @param {number[]} costs
  * @return {number}
  */
 var mincostTickets = function (days, costs) {
-  const n = days[days.length - 1];
-  const dp = Array(n + 1).fill(0);
-  let i = 0;
-  for (let day = 1; day <= n; day++) {
-    if (day === days[i]) {
+  const lastDay = days[days.length - 1];
+  const dp = new Array(lastDay + 1).fill(0);
+  let dayIndex = 0;
+  for (let day = 1; day <= lastDay; day++) {
+    if (day === days[dayIndex]) {
       const costDayPass = dp[day - 1] + costs[0];
       const costWeekPass = dp[Math.max(0, day - 7)] + costs[1];
       const costMonthPass = dp[Math.max(0, day - 30)] + costs[2];
       dp[day] = Math.min(costDayPass, costWeekPass, costMonthPass);
-      i++;
+      dayIndex++;
     } else {
       dp[day] = dp[day - 1];
     }
   }
-  return dp[n];
+  return dp[lastDay];
 };
 
 var days = [1, 4, 6, 7, 8, 20],
