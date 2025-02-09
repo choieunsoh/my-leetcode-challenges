ba2d1260c1a94c576ed6198565e955ef03e0624a
Kiattipong Kamonrat <marsimadz@hotmail.com>
2025-02-09 12:56:02 +0700

    ✨ feat(contest): add weekly contest #436

---
+++ Weekly/431-440/436/app1.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+var sortMatrix = function (grid) {
+  const n = grid.length;
+  const diagonals = Array.from({ length: 2 * n - 1 }, () => []);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      diagonals[i - j + n - 1].push(grid[i][j]);
+    }
+  }
+
+  for (let i = 0; i < diagonals.length; i++) {
+    if (i < n - 1) {
+      diagonals[i].sort((a, b) => b - a);
+    } else {
+      diagonals[i].sort((a, b) => a - b);
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      grid[i][j] = diagonals[i - j + n - 1].pop();
+    }
+  }
+  return grid;
+};
+
+var grid = [
+  [1, 7, 3],
+  [9, 8, 2],
+  [4, 5, 6],
+];
+var expected = [
+  [8, 2, 3],
+  [9, 6, 7],
+  [4, 5, 1],
+];
+var result = sortMatrix(grid);
+console.log(result, result.join() === expected.join());
+
+var grid = [
+  [0, 1],
+  [1, 2],
+];
+var expected = [
+  [2, 1],
+  [1, 0],
+];
+var result = sortMatrix(grid);
+console.log(result, result.join() === expected.join());
+
+var grid = [[1]];
+var expected = [[1]];
+var result = sortMatrix(grid);
+console.log(result, result.join() === expected.join());
