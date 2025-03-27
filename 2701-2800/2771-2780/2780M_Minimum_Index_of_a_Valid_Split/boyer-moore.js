9932b72b9552e3feaedb89fbc04d395de80fcb85
Kiattipong Kamonrat <marsimadz@hotmail.com>
2025-03-27 07:24:16 +0700

    ✨ feat(hash-table): add 2780. Minimum Index of a Valid Split - JS

--- 2701-2800/2771-2780/2780M_Minimum_Index_of_a_Valid_Split/boyer-moore.js
+++ 2701-2800/2771-2780/2780M_Minimum_Index_of_a_Valid_Split/boyer-moore.js
@@ -1,41 +1,42 @@
 // 2780. Minimum Index of a Valid Split
 // https://leetcode.com/problems/minimum-index-of-a-valid-split/
-// T.C: O(n)
-// S.C: O(n)
+// Boyer-Moore Majority Voting Algorithm
+// T.C.: O(n)
+// S.C.: O(1)
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var minimumIndex = function (nums) {
-  const rightFreq = new Map();
-  for (const num of nums) {
-    const count = rightFreq.get(num) ?? 0;
-    rightFreq.set(num, count + 1);
-  }
-
+  let [majority, totalFreq] = majorityElement(nums);
+  let freq = 0;
   const n = nums.length;
-  const leftFreq = new Map();
   for (let i = 0; i < n; i++) {
-    const num = nums[i];
-    let count = leftFreq.get(num) ?? 0;
-    leftFreq.set(num, count + 1);
-    count = rightFreq.get(num) ?? 0;
-    if (count === 1) {
-      rightFreq.delete(num);
-    } else {
-      rightFreq.set(num, count - 1);
+    if (majority === nums[i]) {
+      freq++;
     }
-
-    if (rightFreq.has(num) && leftFreq.has(num)) {
-      const left = leftFreq.get(num) * 2 > i + 1;
-      const right = rightFreq.get(num) * 2 > n - i - 1;
-      if (left && right) {
-        return i;
-      }
+    if (freq * 2 > i + 1 && (totalFreq - freq) * 2 > n - (i + 1)) {
+      return i;
     }
   }
-
   return -1;
+
+  function majorityElement(nums) {
+    // pass1: find majority element
+    let count = 0;
+    let ele = nums[0];
+    nums.forEach((num) => {
+      if (count === 0) {
+        ele = num;
+      }
+      count += ele === num ? 1 : -1;
+    });
+
+    // pass2: find freq of majority element
+    count = 0;
+    nums.forEach((num) => (count += ele === num ? 1 : 0));
+    return [ele, count];
+  }
 };
 
 var nums = [1, 2, 2, 2];
