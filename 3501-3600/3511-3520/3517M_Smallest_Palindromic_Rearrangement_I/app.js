b36453ba91376cd5a10a6a8c35b231202ea81f99
Kiattipong Kamonrat <marsimadz@hotmail.com>
2025-04-15 09:38:13 +0700

    ✨ feat(string): add 3517. Smallest Palindromic Rearrangement I - JS

---
+++ 3501-3600/3511-3520/3517M_Smallest_Palindromic_Rearrangement_I/app.js
@@ -0,0 +1,51 @@
+// 3517. Smallest Palindromic Rearrangement I
+// https://leetcode.com/problems/smallest-palindromic-rearrangement-i/description/
+// T.C.: O(n)
+// S.C.: O(1)
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestPalindrome = function (s) {
+  const a = 'a'.charCodeAt(0);
+  const counts = new Array(26).fill(0);
+  for (const ch of s) {
+    counts[ch.charCodeAt(0) - a]++;
+  }
+  const n = s.length;
+
+  let prefix = '';
+  let suffix = '';
+  let middle = '';
+  for (let i = 0; i < 26; i++) {
+    if (counts[i] === 0) continue;
+
+    const ch = String.fromCharCode(i + a);
+    const count = counts[i];
+    if (count % 2 === 1) {
+      middle = ch;
+    }
+
+    const half = count >> 1;
+    const str = ch.repeat(half);
+    prefix += str;
+    suffix = str + suffix;
+  }
+
+  return prefix + middle + suffix;
+};
+
+var s = 'z';
+var expected = 'z';
+var result = smallestPalindrome(s);
+console.log(result, result === expected);
+
+var s = 'babab';
+var expected = 'abbba';
+var result = smallestPalindrome(s);
+console.log(result, result === expected);
+
+var s = 'daccad';
+var expected = 'acddca';
+var result = smallestPalindrome(s);
+console.log(result, result === expected);
