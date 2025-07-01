fb06d1677b1223c318d81a1f23cb46ee13e9c2b9
Tod Kiattipong <marsimadz@hotmail.com>
2025-07-01 07:49:16 +0700

    ✨ feat(string): implement 3330. Find the Original Typed String I - JS

---
+++ 3301-3400/3321-3330/3330E_Find_the_Original_Typed_String_I/regex.js
@@ -0,0 +1,31 @@
+// 3330. Find the Original Typed String I
+// https://leetcode.com/problems/find-the-original-typed-string-i/description/
+// T.C.: O(n)
+// S.C.: O(1)
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var possibleStringCount = function (word) {
+  return word.match(/(.)\1*/g).reduce((sum, group) => sum + group.length - 1, 1);
+};
+
+var word = 'abbcccc';
+var expected = 5;
+var result = possibleStringCount(word);
+console.log(result, result === expected);
+
+var word = 'abcd';
+var expected = 1;
+var result = possibleStringCount(word);
+console.log(result, result === expected);
+
+var word = 'aaaa';
+var expected = 4;
+var result = possibleStringCount(word);
+console.log(result, result === expected);
+
+var word = 'ebe';
+var expected = 1;
+var result = possibleStringCount(word);
+console.log(result, result === expected);
