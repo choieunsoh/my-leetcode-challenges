24c225d97a5ad98b5897b937977bf3071a7d865f
Tod Kiattipong <marsimadz@hotmail.com>
2025-07-14 07:31:15 +0700

    ✨ feat(linked-list): add solution for 1290. Convert Binary Number in a Linked List to Integer - JS

---
+++ 1201-1300/1281-1290/1290E_Convert_Binary_Number_in_a_Linked_List_to_Integer/app2.js
@@ -0,0 +1,39 @@
+// 1290. Convert Binary Number in a Linked List to Integer
+// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+// T.C.: O(n)
+// S.C.: O(1)
+const { ListNode, createList, toArray } = require('../../../_utils/list');
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function (head) {
+  let value = 0;
+  while (head) {
+    value = 2 * value + head.val;
+    head = head.next;
+  }
+  return value;
+};
+
+var head = createList([1, 0, 1]);
+var expected = 5;
+var result = getDecimalValue(head);
+console.log(result, result === expected);
+
+var head = createList([0]);
+var expected = 0;
+var result = getDecimalValue(head);
+console.log(result, result === expected);
+
+var head = createList([1, 0, 0, 0]);
+var expected = 8;
+var result = getDecimalValue(head);
+console.log(result, result === expected);
