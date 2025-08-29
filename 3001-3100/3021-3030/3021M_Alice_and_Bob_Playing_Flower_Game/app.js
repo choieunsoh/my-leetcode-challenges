20e90897a8ce3585c7375836d476d40c55d5683e
Tod Kiattipong <marsimadz@hotmail.com>
2025-08-29 07:53:43 +0700

    ✨ feat(math): add solution for 3021. Alice and Bob Playing Flower Game - JS

---
+++ 3001-3100/3021-3030/3021M_Alice_and_Bob_Playing_Flower_Game/app.js
@@ -0,0 +1,40 @@
+// 3021. Alice and Bob Playing Flower Game
+// https://leetcode.com/problems/alice-and-bob-playing-flower-game/description/
+// T.C.: O(1)
+// S.C.: O(1)
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var flowerGame = function (n, m) {
+  const xOdd = ((n + 1) / 2) | 0;
+  const yEven = (m / 2) | 0;
+  const xEven = (n / 2) | 0;
+  const yOdd = ((m + 1) / 2) | 0;
+  return xOdd * yEven + xEven * yOdd;
+};
+
+var n = 3,
+  m = 2;
+var expected = 3;
+var result = flowerGame(n, m);
+console.log(result, result === expected);
+
+var n = 1,
+  m = 1;
+var expected = 0;
+var result = flowerGame(n, m);
+console.log(result, result === expected);
+
+var n = 4,
+  m = 3;
+var expected = 6;
+var result = flowerGame(n, m);
+console.log(result, result === expected);
+
+var n = 4,
+  m = 6;
+var expected = 12;
+var result = flowerGame(n, m);
+console.log(result, result === expected);
