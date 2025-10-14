0845250e39ffe2f48d58117e98815cbb7a15ea87
Tod Kiattipong <marsimadz@hotmail.com>
2025-10-14 12:50:39 +0700

    ✨ feat(math): add solution for 2427. Number of Common Factors - JS

---
+++ 2401-2500/2421-2430/2427E_Number_of_Common_Factors/app.js
@@ -0,0 +1,35 @@
+// 2427. Number of Common Factors
+// https://leetcode.com/problems/number-of-common-factors/
+// T.C.: O(log(min(a,b)))
+// S.C.: O(1)
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function (a, b) {
+  const g = gcd(a, b);
+  let count = 0;
+  for (let i = 1; i <= g; i++) {
+    if (g % i === 0) {
+      count++;
+    }
+  }
+  return count;
+
+  function gcd(a, b) {
+    return b ? gcd(b, a % b) : a;
+  }
+};
+
+var a = 12,
+  b = 6;
+var expected = 4;
+var result = commonFactors(a, b);
+console.log(result, result === expected);
+
+var a = 25,
+  b = 30;
+var expected = 2;
+var result = commonFactors(a, b);
+console.log(result, result === expected);
