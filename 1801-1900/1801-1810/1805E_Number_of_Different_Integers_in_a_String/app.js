e2558d4b8aa9bd515400c25646e42ade63f2ade0
Tod Kiattipong <marsimadz@hotmail.com>
2025-11-01 09:30:42 +0700

    ✨ feat(string): add solution for 1805. Number of Different Integers in a String - JS

---
+++ 1801-1900/1801-1810/1805E_Number_of_Different_Integers_in_a_String/app.js
@@ -0,0 +1,34 @@
+// 1805. Number of Different Integers in a String
+// https://leetcode.com/problems/number-of-different-integers-in-a-string/
+// T.C.: O(n)
+// S.C.: O(n)
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numDifferentIntegers = function (word) {
+  // Or /\D/ or /[^0-9]
+  const nums = word.split(/[a-z]/).filter(Boolean).map(BigInt);
+  return new Set(nums).size;
+};
+
+var word = 'a123bc34d8ef34';
+var expected = 3;
+var result = numDifferentIntegers(word);
+console.log(result, result === expected);
+
+var word = 'leet1234code234';
+var expected = 2;
+var result = numDifferentIntegers(word);
+console.log(result, result === expected);
+
+var word = 'a1b01c001';
+var expected = 1;
+var result = numDifferentIntegers(word);
+console.log(result, result === expected);
+
+var word =
+  '2393706880236110407059624696967828762752651982730115221690437821508229419410771541532394006597463715513741725852432559057224478815116557380260390432211227579663571046845842281704281749571110076974264971989893607137140456254346955633455446057823738757323149856858154529105301197388177242583658641529908583934918768953462557716z97438020429952944646288084173334701047574188936201324845149110176716130267041674438237608038734431519439828191344238609567530399189316846359766256507371240530620697102864238792350289978450509162697068948604722646739174590530336510475061521094503850598453536706982695212493902968251702853203929616930291257062173c79487281900662343830648295410';
+var expected = 3;
+var result = numDifferentIntegers(word);
+console.log(result, result === expected);
