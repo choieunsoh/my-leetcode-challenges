6a2092099cdff6efbb3610d3c3ce43e0dcfbc061
Tod Kiattipong <marsimadz@hotmail.com>
2025-12-21 13:18:01 +0700

    ✨ feat(database): add solution for 620. Not Boring Movies - SQL

---
+++ 0601-0700/611-620/620E_Not_Boring_Movies/app.sql
@@ -0,0 +1,16 @@
+# 620. Not Boring Movies
+# https://leetcode.com/problems/not-boring-movies/description/
+
+WITH Cinema (id, movie, description, rating) AS (
+  VALUES
+    (1, 'War', 'great 3D', 8.9),
+    (2, 'Science', 'fiction', 8.5),
+    (3, 'irish', 'boring', 6.2),
+    (4, 'Ice song', 'Fantacy', 8.6),
+    (5, 'House card', 'Interesting', 9.1)
+)
+SELECT id, movie, description, rating
+FROM Cinema
+WHERe description <> 'boring'
+AND id % 2 = 1
+ORDER BY rating DESC;
