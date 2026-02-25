dea76e1e2b9d7b0fed7567230c555579de0e7692
Tod Kiattipong <marsimadz@hotmail.com>
2026-02-25 08:00:27 +0700

    ✨ feat(sorting): add solution for 1356. Sort Integers by The Number of 1 Bits - JS

---
+++ 1301-1400/1351-1360/1356E_Sort_Integers_by_The_Number_of_1_Bits/app4.js
@@ -0,0 +1,42 @@
+// 1356. Sort Integers by The Number of 1 Bits
+// https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/
+// Brian Kerninghan's Algorithm
+// T.C.: O(n log n)
+// S.C.: O(n)
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function (arr) {
+  const findWeight = (num) => {
+    let weight = 0;
+
+    while (num > 0) {
+      weight++;
+      num &= num - 1;
+    }
+
+    return weight;
+  };
+
+  const comparator = (a, b) => {
+    const weightA = findWeight(a);
+    const weightB = findWeight(b);
+    if (weightA === weightB) {
+      return a - b;
+    }
+    return weightA - weightB;
+  };
+
+  return arr.slice().sort(comparator);
+};
+
+var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var expected = [0, 1, 2, 4, 8, 3, 5, 6, 7];
+var result = sortByBits(arr);
+console.log(result, result.join() === expected.join());
+
+var arr = [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1];
+var expected = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024];
+var result = sortByBits(arr);
+console.log(result, result.join() === expected.join());
