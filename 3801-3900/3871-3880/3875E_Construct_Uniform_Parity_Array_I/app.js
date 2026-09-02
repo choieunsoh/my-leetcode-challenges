33908dfdb0d9f87d53ef8bcbb0a2ee3f9a556870
Tod Kiattipong <marsimadz@hotmail.com>
2026-09-02 07:49:16 +0700

    feat(math): add solution for 3875. Construct Uniform Parity Array I - JS

---
+++ 3801-3900/3871-3880/3875E_Construct_Uniform_Parity_Array_I/app.js
@@ -0,0 +1,21 @@
+// 3875. Construct Uniform Parity Array I
+// https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+// T.C.: O(1)
+// S.C.: O(1)
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1) {
+  return true;
+};
+
+var nums1 = [2, 3];
+var expected = true;
+var result = uniformArray(nums1);
+console.log(result, result === expected);
+
+var nums1 = [4, 6];
+var expected = true;
+var result = uniformArray(nums1);
+console.log(result, result === expected);
